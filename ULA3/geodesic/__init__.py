96e009b05931f69a271bd40c8606577424aff728
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2013-08-16 13:28:49 +1000

    Modified the scene centre point retrievals.

--- ULA3/geodesic/__init__.py
+++ ULA3/geodesic/__init__.py
@@ -254,6 +254,7 @@ class Satellite(object):
         self.BAND_TYPES['REFLECTIVE'] = []
         self.BAND_TYPES['THERMAL'] = []
         self.BAND_TYPES['PANCHROMATIC'] = []
+        self.BAND_TYPES['ATMOSPHERE'] = []
         self.rgb_bands = []
 
         self._tle_path_dict = {} # Dict to hold any TLE paths looked up by date
@@ -474,9 +475,12 @@ def eval_centre_data(scene_dataset, sat_ephem):
         'day_of_year': doy,
         'decimal_hour': dec_datetime.hour_fraction(scene_dataset.scene_centre_datetime) + scene_dataset.scene_centre_datetime.hour,
         'decimal_day': dec_datetime.day_fraction(scene_dataset.scene_centre_datetime) + doy,
-        'lon': float(scene_dataset.scene_centre_long),
-        'lat': float(scene_dataset.scene_centre_lat)
+        'lon': float(scene_dataset.lonlats['CENTRE'][0]),
+        'lat': float(scene_dataset.lonlats['CENTRE'][1])
     }
+        #replaced the following in the above dictionary
+        #'lon': float(scene_dataset.scene_centre_long),
+        #'lat': float(scene_dataset.scene_centre_lat)
 
     # CONFIG.debug HACK
     # Adjust scene centre to match EQR test input
