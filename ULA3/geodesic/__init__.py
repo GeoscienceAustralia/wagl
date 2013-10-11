9cec1426e7211a52b09995150b7fb528e9b7d985
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2013-10-11 17:02:37 +1100

    Fixed a call to numpy.meshgrid. Different versions of numPy would return datatypes if called as mg = numpy.meshgrid(). Now called as mx, my = numpy.meshgrid().

--- ULA3/geodesic/__init__.py
+++ ULA3/geodesic/__init__.py
@@ -1395,7 +1395,8 @@ def eval_sat_grids(
     yi = numpy.linspace(ygr[1], ygr[0], nrows)  # Max-to-min
 
     xy = (_x, _y)
-    mg = numpy.meshgrid(xi, yi)
+    mx, my = numpy.meshgrid(xi, yi)
+    mg = (mx, my)
 
     # Cull out-of-scene sample points for bspl/spline interpolation.
 
