77f6471d3608bb3eb2ca787105f2d57a22e0aa5f
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-04-29 14:29:34 +1000

    Changed L1T class ref to NBAR.

--- image_processor/pqa/calc_pqa_masks/acca_cloud_masking.py
+++ image_processor/pqa/calc_pqa_masks/acca_cloud_masking.py
@@ -742,6 +742,6 @@ def process(subprocess_list=[], resume=False):
         logger.debug('ACCA Not Run! %s sensor not configured for the ACCA algorithm.'%nbar_input_dataset.sensor)
         logfile = open("ACCA_LOGFILE.txt", 'w', buffering=0)
         logfile.write("Test Not Run!\n")
-        logfile.write("%s sensor not configured for the ACCA algorithm."%l1t_input_dataset.sensor)
+        logfile.write("%s sensor not configured for the ACCA algorithm."%nbar_input_dataset.sensor)
         logfile.close()
 
