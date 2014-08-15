476f1c366c10a9a60b573f262ff92e51a939bd8c
Dale Roberts <dale.o.roberts@gmail.com>
2014-08-15 14:40:31 +1000

    Tweaks to setup.py

--- ULA3/setup.py
+++ ULA3/setup.py
@@ -1,18 +1,13 @@
 from numpy.distutils.core import Extension, setup
 
-EXT_OPTIONS = {
-#    'f2py_options': ['-assume byterecl']
-}
-
 setup(name='ULA3',
       ext_modules=[
-          Extension(name='_shade_main_landsat_pixel',
-                    sources=['shade_main_landsat_pixel.f90'],
-                    **EXT_OPTIONS),
+          Extension(name='filter',
+                    sources=['filter.f90']),
+           Extension(name='_shade_main_landsat_pixel',
+                    sources=['shade_main_landsat_pixel.f90']),
           Extension(name='_slope_pixelsize_newpole',
-                    sources=['slope_pixelsize_newpole.f90'],
-                    **EXT_OPTIONS),
+                    sources=['slope_pixelsize_newpole.f90']),
           Extension(name='_brdf_terrain_newdiff_all',
-                    sources=['brdf_terrain_newdiff_all.f90'],
-                    **EXT_OPTIONS)
+                    sources=['brdf_terrain_newdiff_all.f90'])
       ])
