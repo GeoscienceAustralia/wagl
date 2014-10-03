79ce55c3259f1d924d377b6d2c9651dc58d1e091
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-10-03 17:10:29 +1000

    Re-worked test cases.

--- ULA3/tests/test_calculate_angles.py
+++ ULA3/tests/test_calculate_angles.py
@@ -13,6 +13,7 @@ import ephem
 
 from EOtools.DatasetDrivers import SceneDataset
 
+from ULA3.geodesic import calculate_angles as ca
 from set_satmod import set_satmod
 from set_times import set_times
 from angle_all import angle
@@ -43,175 +44,38 @@ def write_img(array, name='', format='ENVI', projection=None, geotransform=None)
 
     outds = None
 
-def calculate_julian_century(datetime):
+
+def find_file(dir, file):
     """
-    Given a datetime object return the julian century from the 2000 epoch.
-    """
-
-    # Convert the scene timestamp to a julian date
-    d = ephem.date(datetime)
-    jdate = ephem.julian_date(d)
-
-    # Get the J2000 epoch
-    epoch = ephem.date((2000, 01, 01, 12.00))
-    j2_epoch = ephem.julian_date(epoch)
-
-    # Note:
-    # This differes from online sources such as
-    # http://www.pietro.org/astro_util_staticdemo/FDetailDateConversions.htm
-    # http://en.wikipedia.org/wiki/Equinox_(celestial_coordinates)
-    # which use:
-    # 2000 + (jdate - epoch) / 365.25
-    century = (jdate - j2_epoch) / 36525
-
-    return century
-
-def setup_spheroid(proj_wkt):
-    """
-    Given a WKT projection string, determine the spheroid paramaters
-    that will be used in calcultating the angle grids.
-    """
-
-    # Initialise the spheroid array
-    spheroid = numpy.zeros((4))
-
-    # Define the spatial reference
-    sr = osr.SpatialReference()
-    sr.ImportFromWkt(proj_wkt)
-
-    # Spheroid major axis
-    spheroid[0] = sr.GetSemiMajor()
-
-    # Inverse flattening
-    spheroid[1] = sr.GetInvFlattening()
-
-    # Eccentricity squared
-    spheroid[2] = 1.0 - (1.0 - 1.0/spheroid[1])**2
-
-    # Earth rotational angular velocity rad/sec
-    # Other sources such as:
-    # http://www.oosa.unvienna.org/pdf/icg/2012/template/WGS_84.pdf
-    # state 0.000072921150 as the mean value
-    spheroid[3] = 0.000072722052
-
-    return spheroid 
-
-def setup_orbital_elements(ephemeral, datetime):
-    """
-    Given an ephemeral object and a datetime object, calculate the
-    satellite orbital paramaters used for calculating the angle grids.
-    """
-
-    ephemeral.compute(datetime)
-    pi = numpy.pi
-    n = ephemeral._n # number or orbits per day
-    s = 24*60*60 # Seconds in a day
-    mu = 398600441800000.0 # Earth Gravitational parameter m^3s^-2
-
-    orbital_elements = numpy.zeros((3))
-
-    # orbital inclination (degrees)
-    orbital_elements[0] = numpy.rad2deg(ephemeral._inc)
-
-    # semi_major radius (m)
-    # http://smallsats.org/2012/12/06/two-line-element-set-tle/
-    orbital_elements[1] = (mu/(2*pi*n/s)**2)**(1./3)
     
-    # angular velocity (rad sec-1)
-    orbital_elements[2] = (2*pi*n)/s
-
-    return orbital_elements
-
-def setup_smodel(centre_lon, centre_lat, spheroid, orbital_elements):
     """
-    Setup the satellite model.
+    fname = os.path.join(dir, file)
+    if os.path.isfile(fname):
+        return fname
+    else:
+        print "Error! file not found: %s" %fname
+        sys.exit(2)
+
+
+def compute_angles(scene_dataset, lon_array, lat_array, npoints=12):
     """
-
-    smodel, istat = set_satmod(centre_lon, centre_lat, spheroid, orbital_elements)
-
-    return smodel
-
-def setup_times(ymin, ymax, spheroid, orbital_elements, smodel, npoints=12):
+    
     """
-    Setup the satellite track times.
-    """
-
-    track, istat = set_times(ymin, ymax, npoints, spheroid, orbital_elements, smodel)
-
-    return track
-
-def calculate_angles(scene_dataset, lon_array, lat_array, npoints=12):
-    """
-    Calcualte the satellite view, satellite azimuth, solar zenith,
-    solar azimuth, and relative aziumth angle grids, as well as the
-    time grid. All grids are output as float32 ENVI files.
-    The CENTRELINE is also calculated and output to disk.
-    """
-
-    prj = scene_dataset.GetProjection()
-    geoT = scene_dataset.GetGeoTransform()
-    centre_datetime = scene_dataset.scene_centre_datetime
-
-    centre_xy = numpy.array(scene_dataset.lonlats['CENTRE'])
-
-    print "Setup the spheroid"
-    spheroid = setup_spheroid(scene_dataset.GetProjection())
-
-    print "Setup the orbital_elements"
-    tle_dir = '/g/data1/v10/eoancillarydata/sensor-specific'
-    sat_ephemeral = scene_dataset.satellite.load_tle(centre_datetime, tle_dir)
-    orbital_elements = setup_orbital_elements(sat_ephemeral, centre_datetime)
-
-    lat_min_max = scene_dataset.get_bounds()[1]
-    hours = scene_dataset.decimal_hour
-
-    print "Calculate the julian century past JD2000"
-    century = calculate_julian_century(centre_datetime)
-
-    # Need something to determine max satellite view angle
-    # Currently not even used in Fuqin's code
-    view_max = 9.0
-
-    print "Setup the smodel"
-    smodel = setup_smodel(centre_xy[0], centre_xy[1], spheroid, orbital_elements)
-
-    print "Setup the times and track info"
-    track = setup_times(lat_min_max[0], lat_min_max[1], spheroid, orbital_elements, smodel, npoints)
-
+    # Get the array dimensions
     dims = lon_array.shape
     cols = dims[1]
     rows = dims[0]
 
-    # Initialise the arrays to hold the angles
-    view = numpy.zeros(dims, dtype='float32')
-    azi = numpy.zeros(dims, dtype='float32')
-    asol = numpy.zeros(dims, dtype='float32')
-    soazi = numpy.zeros(dims, dtype='float32')
-    rela_angle = numpy.zeros(dims, dtype='float32')
-    time = numpy.zeros(dims, dtype='float32')
+    # Initialise the satellite maximum view angle
+    view_max = 9.0
 
-    # Set to null value
-    view[:] = -999
-    azi[:] = -999
-    asol[:] = -999
-    soazi[:] = -999
-    rela_angle[:] = -999
-    time[:] = -999
+    # Get the angles, time, & satellite track coordinates
+    (satellite_zenith, satellite_azimuth, solar_zenith, 
+     solar_azimuth, relative_azimuth, time,
+     Y_cent, X_cent, N_cent) = ca.calculate_angles(scene_dataset, lon_array,
+                                                   lat_array, npoints=12)
 
-    # Initialise centre line variables
-    Y_cent = numpy.arange(1,rows+1).astype('float32')
-    X_cent = numpy.zeros((rows), dtype='float32')
-    N_cent = numpy.zeros((rows), dtype='float32')
-
-    print "Calculting the angles by looping over the rows"
-    # Loop over each row
-    for i in range(rows):
-        istat = angle(cols, rows, i+1, lat_array[i], lon_array[i], spheroid,
-                      orbital_elements, hours, century, npoints, smodel, track,
-                      view[i], azi[i], asol[i], soazi[i], rela_angle[i],
-                      time[i], X_cent, N_cent)
-
-    # Angle file names
+    # Define the output file names
     sat_view_zenith_fname = 'SAT_V.bin'
     sat_azimuth_fname = 'SAT_AZ.bin'
     solar_zenith_fname = 'SOL_Z.bin'
@@ -219,49 +83,31 @@ def calculate_angles(scene_dataset, lon_array, lat_array, npoints=12):
     relative_azimuth_fname = 'REL_AZ.bin'
     time_fname = 'TIME.bin'
 
-    print "Writing the angle images"
-
     print "Writing satelite view zenith angle: %s" %sat_view_zenith_fname
-    write_img(view, sat_view_zenith_fname, projection=prj, geotransform=geoT)
+    write_img(satellite_zenith, sat_view_zenith_fname, projection=prj,
+              geotransform=geoT)
 
     print "Writing satellite azimuth angle: %s" %sat_azimuth_fname
-    write_img(azi, sat_azimuth_fname, projection=prj, geotransform=geoT)
+    write_img(satellite_azimuth, sat_azimuth_fname, projection=prj,
+              geotransform=geoT)
 
     print "Writing solar zenith angle: %s" %solar_zenith_fname
-    write_img(asol, solar_zenith_fname, projection=prj, geotransform=geoT)
+    write_img(solar_zenith, solar_zenith_fname, projection=prj,
+              geotransform=geoT)
 
     print "Writing solar azimith angle: %s" %solar_azimuth_fname
-    write_img(soazi, solar_azimuth_fname, projection=prj, geotransform=geoT)
+    write_img(solar_azimuth, solar_azimuth_fname, projection=prj,
+              geotransform=geoT)
 
     print "Writing relative azimuth angle: %s" %relative_azimuth_fname
-    write_img(rela_angle, relative_azimuth_fname, projection=prj, geotransform=geoT)
+    write_img(relative_azimuth, relative_azimuth_fname, projection=prj,
+              geotransform=geoT)
 
     print "Writing time array: %s" %time_fname
     write_img(time, time_fname, projection=prj, geotransform=geoT)
 
     del view, azi, asol, soazi, rela_angle, time
     gc.collect()
-
-    print "Computing the centreline"
-    # Centreline
-    # here need code to write the track in the image as an ascii file
-    # if more than one pixel in a line was a track point the coordinates
-    # are averaged
-    wh = N_cent > 1.5
-    X_cent[wh] = X_cent[wh] / N_cent[wh]
-
-    # check whether there is no centre pixel in the line. It is assumed that
-    # at least the adjacent lines have pixel
-    wh = N_cent < 0.5
-    temp = X_cent[0:2].copy()
-    X_cent[wh] = numpy.roll(X_cent, 1)[wh]
-    # Account for first element potentially being changed with the last element
-    if wh[0]:
-        X_cent[0] = temp[1]
-
-    # Convert X & Y centre points to integers (basically array co-ordinates)
-    Y_cent = numpy.rint(Y_cent)
-    X_cent = numpy.rint(X_cent)
 
     print "Writing out the centreline file"
     # Write the centreline to disk
@@ -273,6 +119,142 @@ def calculate_angles(scene_dataset, lon_array, lat_array, npoints=12):
         outf.write('%i, %i, %f\n'%(Y_cent[r], X_cent[r], N_cent[r]))
 
     outf.close()
+
+
+class CorrectFilenames(unittest.TestCase):
+    """
+    Tests to ensure we have all the correct files on disk before
+    proceeding with array comparisons.
+    """
+
+    def __init__(self):
+        """
+        
+        """
+
+        self.reference_dir = reference_dir
+        self.test_dir = test_dir
+
+        self.fname_sat_v = 'SAT_V.bin'
+        self.fname_sat_az = 'SAT_AZ.bin'
+        self.fname_sol_z = 'SOL_Z.bin'
+        self.fname_sol_az = 'SOL_AZ.bin'
+        self.fname_rel_az = 'REL_AZ.bin'
+        self.fname_time = 'TIME.bin'
+        self.fname_centreline = 'CENTRELINE'
+
+    def test_centreline_ref(self):
+        """
+        Check that reference CENTRELINE text file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_centreline)
+        self.assertIs(os.path.exists(fname), True,
+                      'Reference file does not exist: %s'%fname)
+
+    def test_centreline_tst(self):
+        """
+        Check that test CENTRELINE text file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_centreline)
+        self.assertIs(os.path.exists(fname), True, 
+                      'Test file does not exist: %s'%fname)
+
+    def test_sat_zenith_ref(self):
+        """
+        Check that the reference SAT_V.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_sat_v)
+        self.assertIs(os.path.exists(fname), True,
+                      'Reference file does not exist: %s'%fname)
+
+    def test_sat_zenith_tst(self):
+        """
+        Check that the test SAT_V.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_sat_v)
+        self.assertIs(os.path.exists(fname), True,
+                      'Test file does not exist: %s'%fname)
+
+    def test_sat_azimuth_ref(self):
+        """
+        Check that the reference SAT_AZ.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_sat_az)
+        self.assertIs(os.path.exists(fname), True,
+                      'Reference file does not exist: %s'%fname)
+
+    def test_sat_azimuth_tst(self):
+        """
+        Check that the test SAT_AZ.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_sat_az)
+        self.assertIs(os.path.exists(fname), True,
+                      'Test file does not exist: %s'%fname)
+
+    def test_sol_zenith_ref(self):
+        """
+        Check that the reference SOL_Z.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_sol_z)
+        self.assertIs(os.path.exists(fname), True,
+                      'Reference file does not exist: %s'%fname)
+
+    def test_sol_zenith_tst(self):
+        """
+        Check that the test SOL_Z.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_sol_z)
+        self.assertIs(os.path.exists(fname), True,
+                      'Test file does not exist: %s'%fname)
+
+    def test_sol_azimuth_ref(self):
+        """
+        Check that the reference SOL_AZ.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_sol_az)
+        self.assertIs(os.path.exists(fname), True,
+                      'Reference file does not exist: %s'%fname)
+
+    def test_sol_azimuth_tst(self):
+        """
+        Check that the test SOL_AZ.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_sol_az)
+        self.assertIs(os.path.exists(fname), True,
+                      'Test file does not exist: %s'%fname)
+
+    def test_rel_azimuth_ref(self):
+        """
+        Check that the reference REL_AZ.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_rel_az)
+        self.assertIs(os.path.exists(fname), True,
+                      'Reference file does not exist: %s'%fname)
+
+    def test_rel_azimuth_tst(self):
+        """
+        Check that the test REL_AZ.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_rel_az)
+        self.assertIs(os.path.exists(fname), True,
+                      'Test file does not exist: %s'%fname)
+
+    def test_time_ref(self):
+        """
+        Check that the reference TIME.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_time)
+        self.assertIs(os.path.exists(fname), True,
+                      'Reference file does not exist: %s'%fname)
+
+    def test_time_tst(self):
+        """
+        Check that the reference TIME.bin image file exists.
+        """
+        fname = os.path.join(self.reference_dir, self.fname_time)
+        self.assertIs(os.path.exists(fname), True,
+                      'Test file does not exist: %s'%fname)
+
 
 #class AnglesOutputsTester(unittest.TestCase):
 class AnglesOutputsTester(npt.TestCase):
@@ -328,17 +310,6 @@ class AnglesOutputsTester(npt.TestCase):
         self.centreline_ref = None
         self.centreline_test = None
         self._read_centreline_files()
-
-    def find_file(self, dir, file):
-        """
-        
-        """
-        fname = os.path.join(dir, file)
-        if os.path.isfile(fname):
-            return fname
-        else:
-            print "Error! file not found: %s" %fname
-            sys.exit(2)
 
     def _read_centreline_files(self):
         """
@@ -614,7 +585,7 @@ if __name__ == '__main__':
     lat_ds = None
 
     print "Starting main routine"
-    calculate_angles(ds, lon_arr, lat_arr)
+    comput_angles(ds, lon_arr, lat_arr)
 
     print "Running unittests on the angle grids and the CENTRELINE file."
     suite = AnglesOutputsTester(reference_dir=nbar_work_dir, test_dir=outdir,
