10bdde7447cc9d5ac39c68b851bfc45699b24431
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-10-07 16:58:24 +1100

    Initialised the unittests procedure.

--- ULA3/tests/test_slope.py
+++ ULA3/tests/test_slope.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
-import sys
-import os
 import argparse
-import gc
+import os
+import sys
 
-import numpy
-import numpy.testing as npt
 from osgeo import gdal
 from osgeo import osr
+import numpy
+import numpy.testing as npt
 
 from EOtools.DatasetDrivers import SceneDataset
-
-from ULA3.tc import run_slope
 from ULA3._gdal_tools.py import Buffers
+from ULA3.tc import run_slope
+from unittesting_tools import find_file
+from unittesting_tools import ParameterisedTestCase
+from unittesting_tools import read_img
+from unittesting_tools import write_img
 
-from general_tools import write_img, read_img, find_file
 
 def calculate_slope_angles(scene_dataset, ref_dir, outdir, pixel_buffer=250):
     """
@@ -42,12 +43,20 @@ def calculate_slope_angles(scene_dataset, ref_dir, outdir, pixel_buffer=250):
 
     slope_results = run_slope(scene_dataset, dsm_data, solar_angle, view_angle, sazi_angle, azi_angle, pixel_buf, is_utm)
 
-    slope_results.dump_arrays(dump_path, scene_dataset, "ENVI", ".bin")
+    slope_results.dump_arrays(dump_path, scene_dataset, "ENVI", ".img")
 
 
-class test_slope_filenames():
+class TestSlopeFilenames(ParameterisedTestCase):
     """
-    
+    Unittests will occur for the following files:
+    mask_self.img
+    slope.img
+    aspect.img
+    incident.img
+    exiting.img
+    azi_incident.img
+    azi_exiting.img
+    rela_slope.img
     """
 
     def __init__(self):
@@ -55,17 +64,14 @@ class test_slope_filenames():
         
         """
 
-        self.reference_dir = reference_dir
-        self.test_dir = test_dir
-
-        self.fname_mask_self = 'mask_self'
-        self.fname_slope = 'slope'
-        self.fname_aspect = 'aspect'
-        self.fname_incident = 'incident'
-        self.fname_exiting = 'exiting'
-        self.fname_azi_incident = 'azi_incident'
-        self.fname_azi_exiting = 'azi_exiting'
-        self.fname_rela_slope = 'rela_slope'
+        self.fname_mask_self     = 'mask_self.img'
+        self.fname_slope         = 'slope.img'
+        self.fname_aspect        = 'aspect.img'
+        self.fname_incident      = 'incident.img'
+        self.fname_exiting       = 'exiting.img'
+        self.fname_azi_indcident = 'azi_incident.img'
+        self.fname_azi_exiting   = 'azi_exiting.img'
+        self.fname_rela_slope    = 'rela_slope.img'
 
     def test_mask_self_ref(self):
         """
@@ -73,7 +79,7 @@ class test_slope_filenames():
         """
         fname = os.path.join(self.reference_dir, self.fname_mask_self)
         self.assertIs(os.path.exists(fname), True,
-                      'Reference file does not exist: %s'%fname)
+                      '_reference file does not exist: %s'%fname)
 
     def test_mask_self_tst(self):
         """
@@ -89,7 +95,7 @@ class test_slope_filenames():
         """
         fname = os.path.join(self.reference_dir, self.fname_slope)
         self.assertIs(os.path.exists(fname), True,
-                      'Reference file does not exist: %s'%fname)
+                      '_reference file does not exist: %s'%fname)
 
     def test_slope_tst(self):
         """
@@ -105,7 +111,7 @@ class test_slope_filenames():
         """
         fname = os.path.join(self.reference_dir, self.fname_aspect)
         self.assertIs(os.path.exists(fname), True,
-                      'Reference file does not exist: %s'%fname)
+                      '_reference file does not exist: %s'%fname)
 
     def test_aspect_tst(self):
         """
@@ -121,7 +127,7 @@ class test_slope_filenames():
         """
         fname = os.path.join(self.reference_dir, self.fname_incident)
         self.assertIs(os.path.exists(fname), True,
-                      'Reference file does not exist: %s'%fname)
+                      '_reference file does not exist: %s'%fname)
 
     def test_incident_tst(self):
         """
@@ -137,7 +143,7 @@ class test_slope_filenames():
         """
         fname = os.path.join(self.reference_dir, self.fname_exiting)
         self.assertIs(os.path.exists(fname), True,
-                      'Reference file does not exist: %s'%fname)
+                      '_reference file does not exist: %s'%fname)
 
     def test_exiting_tst(self):
         """
@@ -153,7 +159,7 @@ class test_slope_filenames():
         """
         fname = os.path.join(self.reference_dir, self.fname_azi_incident)
         self.assertIs(os.path.exists(fname), True,
-                      'Reference file does not exist: %s'%fname)
+                      '_reference file does not exist: %s'%fname)
 
     def test_azi_incident_tst(self):
         """
@@ -169,7 +175,7 @@ class test_slope_filenames():
         """
         fname = os.path.join(self.reference_dir, self.fname_azi_exiting)
         self.assertIs(os.path.exists(fname), True,
-                      'Reference file does not exist: %s'%fname)
+                      '_reference file does not exist: %s'%fname)
 
     def test_azi_exiting_tst(self):
         """
@@ -185,7 +191,7 @@ class test_slope_filenames():
         """
         fname = os.path.join(self.reference_dir, self.fname_rela_slope)
         self.assertIs(os.path.exists(fname), True,
-                      'Reference file does not exist: %s'%fname)
+                      '_reference file does not exist: %s'%fname)
 
     def test_rela_slope_tst(self):
         """
@@ -196,7 +202,160 @@ class test_slope_filenames():
                       'Test file does not exist: %s'%fname)
 
 
-class test_slope_angles():
+class TestSlopeAngles(ParameterisedTestCase):
+    """
+    Unittests will occur for the following files:
+    mask_self.img
+    slope.img
+    aspect.img
+    incident.img
+    exiting.img
+    azi_incident.img
+    azi_exiting.img
+    rela_slope.img
+    """
+
+    def __init__(self):
+        """
+        
+        """
+        self.fname_mask_self     = 'mask_self.img'
+        self.fname_slope         = 'slope.img'
+        self.fname_aspect        = 'aspect.img'
+        self.fname_incident      = 'incident.img'
+        self.fname_exiting       = 'exiting.img'
+        self.fname_azi_indcident = 'azi_incident.img'
+        self.fname_azi_exiting   = 'azi_exiting.img'
+        self.fname_rela_slope    = 'rela_slope.img'
+
+    def test_mask_self(self):
+        """
+        Test the self mask array.
+        """
+
+        # Get the filenames for both the reference and test files
+        ref_fname = find_file(self.reference_dir, self.fname_mask_self)
+        test_fname = find_file(self.test_dir, self.fname_mask_self)
+
+        # Get the image data
+        ref_img = read_img(ref_fname)
+        test_img = read_img(test_fname)
+
+        self.assertIsNone(npt.assert_almost_equal(test_img, ref_img,
+                                                  decimal=self.dec_precision))
+
+    def test_slope(self):
+        """
+        Test the slope array.
+        """
+
+        # Get the filenames for both the reference and test files
+        ref_fname = find_file(self.reference_dir, self.fname_slope)
+        test_fname = find_file(self.test_dir, self.fname_slope)
+
+        # Get the image data
+        ref_img = read_img(ref_fname)
+        test_img = read_img(test_fname)
+
+        self.assertIsNone(npt.assert_almost_equal(test_img, ref_img,
+                                                  decimal=self.dec_precision))
+
+    def test_aspect(self):
+        """
+        Test the aspect array.
+        """
+
+        # Get the filenames for both the reference and test files
+        ref_fname = find_file(self.reference_dir, self.fname_aspect)
+        test_fname = find_file(self.test_dir, self.fname_aspect)
+
+        # Get the image data
+        ref_img = read_img(ref_fname)
+        test_img = read_img(test_fname)
+
+        self.assertIsNone(npt.assert_almost_equal(test_img, ref_img,
+                                                  decimal=self.dec_precision))
+
+    def test_incident(self):
+        """
+        Test the incident array.
+        """
+
+        # Get the filenames for both the reference and test files
+        ref_fname = find_file(self.reference_dir, self.fname_incident)
+        test_fname = find_file(self.test_dir, self.fname_incident)
+
+        # Get the image data
+        ref_img = read_img(ref_fname)
+        test_img = read_img(test_fname)
+
+        self.assertIsNone(npt.assert_almost_equal(test_img, ref_img,
+                                                  decimal=self.dec_precision))
+
+    def test_exiting(self):
+        """
+        Test the exiting array.
+        """
+
+        # Get the filenames for both the reference and test files
+        ref_fname = find_file(self.reference_dir, self.fname_exiting)
+        test_fname = find_file(self.test_dir, self.fname_exiting)
+
+        # Get the image data
+        ref_img = read_img(ref_fname)
+        test_img = read_img(test_fname)
+
+        self.assertIsNone(npt.assert_almost_equal(test_img, ref_img,
+                                                  decimal=self.dec_precision))
+
+    def test_azimuth_indcident(self):
+        """
+        Test the azimuth incident array.
+        """
+
+        # Get the filenames for both the reference and test files
+        ref_fname = find_file(self.reference_dir, self.fname_azi_indcident)
+        test_fname = find_file(self.test_dir, self.fname_azi_indcident)
+
+        # Get the image data
+        ref_img = read_img(ref_fname)
+        test_img = read_img(test_fname)
+
+        self.assertIsNone(npt.assert_almost_equal(test_img, ref_img,
+                                                  decimal=self.dec_precision))
+
+    def test_azimuth_exiting(self):
+        """
+        Test the azimuth exiting array.
+        """
+
+        # Get the filenames for both the reference and test files
+        ref_fname = find_file(self.reference_dir, self.fname_azi_exiting)
+        test_fname = find_file(self.test_dir, self.fname_azi_exiting)
+
+        # Get the image data
+        ref_img = read_img(ref_fname)
+        test_img = read_img(test_fname)
+
+        self.assertIsNone(npt.assert_almost_equal(test_img, ref_img,
+                                                  decimal=self.dec_precision))
+
+    def test_relative_slope(self):
+        """
+        Test the relative slope array.
+        """
+
+        # Get the filenames for both the reference and test files
+        ref_fname = find_file(self.reference_dir, self.fname_rela_slope)
+        test_fname = find_file(self.test_dir, self.fname_rela_slope)
+
+        # Get the image data
+        ref_img = read_img(ref_fname)
+        test_img = read_img(test_fname)
+
+        self.assertIsNone(npt.assert_almost_equal(test_img, ref_img,
+                                                  decimal=self.dec_precision))
+
 
 if __name__ == '__main__':
 
@@ -208,6 +367,55 @@ if __name__ == '__main__':
     parser.add_argument('--outdir', required=True, help='A directory path that will contain the output files.')
     parser.add_argument('--dec_precision', default=4, help='The decimal precision used for array comparison')
     parser.add_argument('--int_precision', default=1, help='The integer precision used for array comparison')
+    parser.add_argument('--compute', action='store_true', help='If set then the slope, aspect, incident, exiting & relative angles will be computed before running the unittests.')
+    parser.add_argument('--buffer', default=250, help='The buffer in pixels to be used in calculating the slope.')
 
     parsed_args = parser.parse_args()
+
+    L1T_dir       = parsed_args.L1T_dir
+    nbar_work_dir = parsed_args.nbar_work_dir
+    outdir        = parsed_args.outdir
+    dec_precision = parsed_args.dec_precision
+    int_precision = parsed_args.int_precision
+    compute       = parsed_args.compute
+    buffer        = parsed_args.buffer
+
+    if compute:
+        # Check the output directory
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+
+        # Get the current directory
+        cwd = os.getcwd()
+
+        # Change to the output directory that will contain the results
+        os.chdir(outdir)
+
+        # Open the L1T dataset
+        ds = SceneDataset(L1T_dir)
+
+        # Compute the angles
+        calculate_slope_angles(ds, nbar_work_dir, outdir, buffer)
+
+        # Close the L1T dataset
+        ds = None
+
+        # Change back to the original directory
+        os.chdir(cwd)
+
+    print "Checking that we have all the reference and test data files neccessary."
+    suite = unittest.TestSuite()
+    suite.addTest(ParameterisedTestCase.parameterise(TestSlopeFilenames,
+                  reference_dir=nbar_work_dir, test_dir=outdir,
+                  decimal_precision=dec_precision,
+                  integer_precision=int_precision)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+    print "Comparing the reference and test slope output files."
+    suite = unittest.TestSuite()
+    suite.addTest(ParameterisedTestCase.parameterise(TestSlopeAngles,
+                  reference_dir=nbar_work_dir, test_dir=outdir,
+                  decimal_precision=dec_precision,
+                  integer_precision=int_precision)
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
