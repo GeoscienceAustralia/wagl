4443184df274ef50d4cf18974d26a590ace86b38
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-11-19 14:59:05 +1100

    Files copied into gaip so as not to break ULA3.

---
+++ gaip/geo2metres_pixel_size.f90
@@ -0,0 +1,71 @@
+! subroutine geo2metres_pixel_size
+subroutine geo2metres_pixel_size(ycent,hx,hy,spheroid, &
+             hx_out,hy_out,istat)
+
+!   integer subroutine to get pixel size for geographic coordinates
+!   depends only on latitude ycent
+!   ycent, hx and hy are in degrees
+!   hx_out and hy_out are in metres
+!   spheroid is wgs84 which is appropriate here
+
+!   * Re-written as an indepentent subroutine by JS, Aug 2014
+!   * Probable overlap with subroutine pixelsize(rlat,dres,dx,dy)???
+!   * pixelsize(rlat,dres,dx,dy) & pixelsize(rlat,dres,dx,dy,pia)
+!     have now been replaced by this routine which itself has
+!     been renamed to geo2metres_pixel_size which describes the
+!     routine a little better.
+!   * The routine now takes a 4 element spheroid parameter rather
+!     than hard coded WGS84 parameters.
+
+!   Inputs:
+!       ycent
+!       hx
+!       hy
+!       spheroid
+!           1. Spheroid major axis
+!           2. Inverse flattening
+!           3. Eccentricity squared
+!           4. Earth rotational angular velocity rad/sec
+!
+!   Outputs:
+!       hx_out
+!       hy_out
+!       istat
+
+    use sys_variables, only : pi, d2r, r2d
+
+    implicit none
+
+    double precision ycent
+    double precision hx, hy, hx_out, hy_out
+
+    double precision spheroid(4)
+    double precision asph, finv
+
+    double precision yc, a_e, b_e, p_e, q_e, Earth_Radius, d_phi
+
+    integer istat
+
+!   Spheroid parameters
+!   Spheroid major axis
+!   Inverse flattening
+    asph = spheroid(1)
+    finv = spheroid(2)
+
+    yc = ycent*d2r
+    a_e = asph
+    b_e = a_e*(1.0d0-1.0d0/finv)
+    p_e = a_e*cos(yc)
+    q_e = b_e*sin(yc)
+    Earth_Radius = sqrt(((a_e*p_e)**2+(b_e*q_e)**2)/(p_e**2+q_e**2))
+    d_phi = (sin(yc))**2+cos(hx*d2r)*(cos(yc))**2
+    d_phi = acos(d_phi)
+
+    hx_out = d_phi*Earth_Radius
+    hy_out = (hy*d2r)*Earth_Radius
+
+    istat = 0
+
+    return
+
+end subroutine geo2metres_pixel_size
---
+++ gaip/slope_pixelsize_newpole.f90
@@ -0,0 +1,127 @@
+SUBROUTINE slope_pixelsize_newpole( &
+    dresx, dresy, spheroid, alat, is_utm, &
+    dem, solar, view, sazi, azi, &
+    mask, theta, phit, it, et, azi_it, azi_et, rela, &
+    nrow_alloc, ncol_alloc, nrow, ncol, ierr)
+! this program is used to calculate slope and aspect angles
+! using Sobel filter and then calculate incident and
+! exiting angles as well as their azimuth angles.
+! note: the row and column of DEM data must be larger
+! than the image (extra each line and column for the four sides.
+! it is needed for sobel filter.
+
+!   Inputs:
+!       spheroid
+!           1. Spheroid major axis
+!           2. Inverse flattening
+!           3. Eccentricity squared
+!           4. Earth rotational angular velocity rad/sec
+
+    real*8 dresx, dresy
+    real*8 spheroid(4)
+	real*8 alat(nrow) ! remember that row starts at 2
+	real*4 dem(nrow, ncol) !
+    real*4 solar(nrow_alloc, ncol_alloc) !
+    real*4 view(nrow_alloc, ncol_alloc) !
+    real*4 sazi(nrow_alloc, ncol_alloc) !
+    real*4 azi(nrow_alloc, ncol_alloc) !
+    integer*2 mask(nrow_alloc, ncol_alloc) !
+    real*4 theta(nrow_alloc, ncol_alloc) !
+    real*4 phit(nrow_alloc, ncol_alloc) !
+    real*4 it(nrow_alloc, ncol_alloc) !
+    real*4 et(nrow_alloc, ncol_alloc) !
+    real*4 azi_it(nrow_alloc, ncol_alloc) !
+    real*4 azi_et(nrow_alloc, ncol_alloc) !
+    real*4 rela(nrow_alloc, ncol_alloc) !
+    logical is_utm
+    integer*4 nrow_alloc, ncol_alloc, nrow, ncol, row, col
+    integer*4 ierr
+
+! internal variables
+    real*8 dx, dy
+    real*8 pi, pia, pib
+    real*8 p, q
+    integer istat
+
+!f2py intent(in) dresx, dresy, spheroid, alat, dem, solar, view, sazi, azi, is_utm
+!f2py intent(out) mask, theta, phit, it, et, azi_it, azi_et, rela
+!f2py intent(out) ierr
+!f2py integer intent(hide),depend(solar) :: nrow_alloc=shape(solar,0), ncol_alloc=shape(solar,1)
+!f2py integer intent(hide),depend(dem) :: nrow=shape(dem,0), ncol=shape(dem,1)
+
+!---------------------------------------------------------------------
+!   upper left coordinator for satellite image.
+!   here we assume that Landsat and DEM have same spatial resolution
+!   and coordinator. The information can be obtained from satellite
+!   header file. DEM header should add an extra pixel.
+    pi=4.0d0*atan(1.0d0)
+    pia=pi/180.0d0
+    pib=180.0d0/pi
+    ierr = 0;
+!   check that the arrays dimensions are correct (relatively)
+    if(ncol_alloc .ne. (ncol-2)) then
+        ierr = 10
+        return
+    endif
+
+    if(nrow_alloc .ne. (nrow-2)) then
+        ierr = 11
+        return
+    endif
+
+    ncol = ncol-2
+    nrow = nrow-2
+
+!   calculate pixel size in meters
+    if(is_utm) then
+        dx = dresx
+        dy = dresy
+    endif
+!-------------------------------------------------------------------
+    do row=2,nrow+1
+        if(.not. is_utm) then
+            call geo2metres_pixel_size(alat(row), dresx, dresy, &
+                                       spheroid, dx, dy, istat)
+        endif
+
+        do col=2,ncol+1
+            mask(row-1, col-1)=1
+            if (sazi(row-1, col-1) .le. -180.0) sazi(row-1, col-1)=sazi(row-1, col-1)+360.0
+            if (sazi(row-1, col-1) .gt.  180.0) sazi(row-1, col-1)=sazi(row-1, col-1)-360.0
+            if ( azi(row-1, col-1) .le. -180.0)  azi(row-1, col-1)= azi(row-1, col-1)+360.0
+            if ( azi(row-1, col-1) .gt.  180.0)  azi(row-1, col-1)= azi(row-1, col-1)-360.0
+            p = &
+                      (dble(dem(row-1,col+1))-dble(dem(row-1,col-1)) + &
+                2.0d0*(dble(dem(row  ,col+1))-dble(dem(row  ,col-1))) + &
+                       dble(dem(row+1,col+1))-dble(dem(row+1,col-1))) / (8.0d0*dx)
+            q = &
+                      (dble(dem(row-1,col-1))-dble(dem(row+1,col-1)) + &
+                2.0d0*(dble(dem(row-1,col  ))-dble(dem(row+1,col  ))) + &
+                       dble(dem(row-1,col+1))-dble(dem(row+1,col+1))) / (8.0d0*dy)
+    !----------------------------------------------------------------
+            theta(row-1, col-1)=sngl(atan(sqrt(p**2+q**2))*pib)
+            phit(row-1, col-1)=sngl(atan2(-p,-q)*pib)
+            if (phit(row-1, col-1) .le. -180.0) phit(row-1, col-1)=phit(row-1, col-1)+360.0
+            if (phit(row-1, col-1) .gt.  180.0) phit(row-1, col-1)=phit(row-1, col-1)-360.0
+            call cal_pole(solar(row-1, col-1), sazi(row-1, col-1), &
+            theta(row-1, col-1), phit(row-1, col-1), it(row-1, col-1), azi_it(row-1, col-1))
+            call cal_pole( view(row-1, col-1),  azi(row-1, col-1), &
+            theta(row-1, col-1), phit(row-1, col-1), et(row-1, col-1), azi_et(row-1, col-1))
+            if (azi_it(row-1, col-1).le. -180.0) azi_it(row-1, col-1)=azi_it(row-1, col-1)+360
+            if (azi_it(row-1, col-1).gt.  180.0) azi_it(row-1, col-1)=azi_it(row-1, col-1)-360
+            if (azi_et(row-1, col-1).le. -180.0) azi_et(row-1, col-1)=azi_et(row-1, col-1)+360
+            if (azi_et(row-1, col-1).gt.  180.0) azi_et(row-1, col-1)=azi_et(row-1, col-1)-360
+            rela(row-1, col-1)=azi_it(row-1, col-1)-azi_et(row-1, col-1)
+    !----------------------------------------------------------------
+            if (rela(row-1, col-1) .le. -180.0) rela(row-1, col-1)=rela(row-1, col-1)+360.0
+            if (rela(row-1, col-1) .gt. 180.0) rela(row-1, col-1)=rela(row-1, col-1)-360.0
+            if (cos(it(row-1, col-1)*pia) .le. 0.0) then
+                mask(row-1, col-1)=0
+            endif
+            if (cos(et(row-1, col-1)*pia) .le.0.0 ) then
+                mask(row-1, col-1)=0
+            endif
+        enddo
+    enddo
+    return
+END SUBROUTINE slope_pixelsize_newpole
