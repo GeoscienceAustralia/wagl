31448324807bb4a3a7be6fbd9884a2fc492bdff6
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-12-10 09:44:51 +1100

    Reorded the imports list.

--- gaip/__init__.py
+++ gaip/__init__.py
@@ -1,13 +1,3 @@
-# Fortran modules (F2Py)
-from set_satmod import set_satmod
-from set_times import set_times
-from angle_all import angle
-from filtering import filter_float as filter
-from _shade_main_landsat_pixel import shade_main_landsat_pixel
-from _slope_pixelsize_newpole import slope_pixelsize_newpole
-from _brdf_terrain_newdiff_all import terrain_correction
-
-# Python imports
 from ancillary import *
 from acquisition import *
 from data import *
@@ -19,5 +9,11 @@ from tle import *
 from brdf import *
 from get_brdf import *
 from calculate_lon_lat_arrays import *
+from set_satmod import set_satmod # F2Py
+from set_times import set_times # F2Py
+from angle_all import angle # F2Py
+from filtering import filter_float as filter # F2Py
+from _shade_main_landsat_pixel import shade_main_landsat_pixel # F2Py
+from _slope_pixelsize_newpole import slope_pixelsize_newpole # F2Py
+from _brdf_terrain_newdiff_all import terrain_correction # F2Py
 from calculate_angles import *
-
