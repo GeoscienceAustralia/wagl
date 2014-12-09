4f2d4dfab9c8bee191dcd5b984025529561a6ad5
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-12-09 16:19:04 +1100

    Reordered the imports to avoid dependency errors.

--- gaip/__init__.py
+++ gaip/__init__.py
@@ -1,13 +1,23 @@
+# Fortran modules (F2Py)
+from set_satmod import set_satmod
+from set_times import set_times
+from angle_all import angle
+from filtering import filter_float as filter
+from _shade_main_landsat_pixel import shade_main_landsat_pixel
+from _slope_pixelsize_newpole import slope_pixelsize_newpole
+from _brdf_terrain_newdiff_all import terrain_correction
+
+# Python imports
 from ancillary import *
 from acquisition import *
 from data import *
 from mtl import *
 from water import *
 from GriddedGeoBox import GriddedGeoBox
-from io import *
-from brdf import *
 from fast_cache import *
-from calculate_lon_lat_arrays import *
 from tle import *
 from brdf import *
 from get_brdf import *
+from calculate_lon_lat_arrays import *
+from calculate_angles import *
+
