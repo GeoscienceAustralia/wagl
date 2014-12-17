93ddc2b82aaf966dcede32f55ecdebb9bd9d6e77
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-12-17 17:00:36 +1100

    Added missing imports. Moved Buffers class from ULA3._gdal_tools.py to run_tc.py. Reworked filter_dsm() to use scipy.ndimage() rather than custom fortran routine. Moved write_new_brdf_file() & write_header_slope_file() from run_tc.py to tc.py.

--- gaip/tc.py
+++ gaip/tc.py
@@ -1,12 +1,92 @@
 import os
 import numpy
-from gaip import filter
+from scipy import ndimage
 from gaip import shade_main_landsat_pixel
 from gaip import slope_pixelsize_newpole
 from gaip import terrain_correction
 from gaip import as_array
+from gaip import write_img
 
 
+def filter_dsm(array):
+    """
+    Applies a gaussian filter to array.
+
+    :param array:
+        A 2D NumPy array.
+
+    :return:
+        A 2D NumPy array.
+    """
+    # Define the kernel
+    kernel = [0.009511, 0.078501, 0.009511, 0.078501, 0.647954, 0.078501,
+        0.009511, 0.078501, 0.009511]
+    kernel = numpy.array(kernel).reshape((3,3))
+
+    filtered = ndimage.convolve(array, kernel)
+    return filtered
+
+
+class Buffers(object):
+    """
+    Holds some value for each side of an image. This was initially
+    created to hold buffer widths (in pixels) for a scene, but does
+    not care about the type of the values passed to the constructer
+    and can hence be used for any type.
+    """
+    def __init__(self, left, right=None, top=None, bottom=None):
+        """
+        Constructor.
+
+        The arguments are copied directly to members of the same name
+        with the restrictions that:
+
+        - if right is None then top and bottom must also be None, and
+
+        - if right is not None then top and bottom must not be none
+          either.
+        """
+        if right is None:
+            msg = "if right is None then top and bottom must also be None"
+            assert top is None and bottom is None, msg
+            self.left = self.right = self.top = self.bottom = left
+        else:
+            msg = ("if right is not None then top and bottom must "
+                   "also not be None")
+            assert top is not None and bottom is not None, msg
+            self.left = left
+            self.right = right
+            self.top = top
+            self.bottom = bottom
+
+    def __str__(self):
+        msg = "Buffers({left}, {right}, {top}, {bottom})"
+        msg = msg.format(left=self.left, right=self.right, top=self.top,
+            bottom=self.bottom)
+        return msg
+
+
+def write_new_brdf_file(file_name, *args):
+    with open(file_name, 'w') as src:
+        out_string = "{0}\n{1} {2} {3}\n{4} {5} {6} {7}\n{8}\n"
+        out_string = out_string.format(*args)
+        src.write(out_string)
+
+
+def write_header_slope_file(file_name, bounds, geobox):
+    with open(file_name, 'w') as output:
+        # get dimensions, resolution and pixel origin
+        rows, cols = geobox.shape
+        res = geobox.res
+        origin = geobox.origin
+
+        # Now output the details
+        output.write("{nr} {nc}\n".format(nr=rows, nc=cols))
+        output.write("{0} {1}\n{2} {3}\n".format(bounds.left, bounds.right,
+            bounds.top, bounds.bottom))
+        output.write("{resy} {resx}\n".format(resx=res[0], resy=res[1]))
+        output.write("{yorigin} {xorigin}\n".format(xorigin=origin[0],
+            yorigin=origin[1]))
 
 
 class FortranError(Exception):
@@ -69,21 +149,21 @@ class SlopeResultSet(object):
             file_extension)
 
         # Write
-        write_img(mask_self, fname_mask_self, format=file_type, geobox=geobox,
-            nodata=-999)
-        write_img(slope, fname_slope, format=file_type, geobox=geobox,
-            nodata=-999)
-        write_img(aspect, fname_aspect, format=file_type, geobox=geobox,
-            nodata=-999)
-        write_img(incident, fname_incident, format=file_type, geobox=geobox,
-            nodata=-999)
-        write_img(exiting, fname_exiting, format=file_type, geobox=geobox,
-            nodata=-999)
-        write_img(azi_incident, fname_azimuth_incident, format=file_type,
+        write_img(self.mask_self, fname_mask_self, format=file_type,
             geobox=geobox, nodata=-999)
-        write_img(azi_exiting, fname_azimuth_exiting, format=file_type,
+        write_img(self.slope, fname_slope, format=file_type, geobox=geobox,
+            nodata=-999)
+        write_img(self.aspect, fname_aspect, format=file_type, geobox=geobox,
+            nodata=-999)
+        write_img(self.incident, fname_incident, format=file_type,
             geobox=geobox, nodata=-999)
-        write_img(rela_slope, fname_relative_slope, format=file_type,
+        write_img(self.exiting, fname_exiting, format=file_type,
+            geobox=geobox, nodata=-999)
+        write_img(self.azi_incident, fname_azimuth_incident, format=file_type,
+            geobox=geobox, nodata=-999)
+        write_img(self.azi_exiting, fname_azimuth_exiting, format=file_type,
+            geobox=geobox, nodata=-999)
+        write_img(self.rela_slope, fname_relative_slope, format=file_type,
             geobox=geobox, nodata=-999)
 
 
@@ -209,9 +289,9 @@ def run_slope(
         msg = msg.format(dtype=solar_zenith.dtype.name)
         raise TypeError(msg)
 
-    if satelltite_zenith.dtype.name != 'float32':
-        msg = 'Satelltite zenith datatype must be float32! Datatype: {dtype}'
-        msg = msg.format(dtype=satelltite_zenith.dtype.name)
+    if satellite_zenith.dtype.name != 'float32':
+        msg = 'Satellite zenith datatype must be float32! Datatype: {dtype}'
+        msg = msg.format(dtype=satellite_zenith.dtype.name)
         raise TypeError(msg)
 
     if solar_azimuth.dtype.name != 'float32':
@@ -236,8 +316,8 @@ def run_slope(
     ncol = cols + 2
     nrow = rows + 2
 
-    dem_dat = DEM[(pix_buf.top-1):-(pix_buf.bottom-1),(pix_buf.left-1):
-        -(pix_buf.right-1)]
+    dem_dat = DEM[(buffer.top-1):-(buffer.bottom-1),(buffer.left-1):
+        -(buffer.right-1)]
     # Check that the dimensions match
     if dem_dat.shape != (nrow, ncol):
         msg = ('DEM index not of correct shape ({row}, {col}) '
@@ -255,7 +335,7 @@ def run_slope(
         dresx, dresy, spheroid, alat, is_utm,
         dem_dat,
         solar_zenith,
-        satelltite_zenith,
+        satellite_zenith,
         solar_azimuth,
         satellite_azimuth)
 
