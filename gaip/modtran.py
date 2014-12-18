94a55baf61e2a9d50f5a800e4b208575938a7748
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-12-18 17:30:32 +1100

    Added a return dict for the bilinear (band,factor) pathname results.

--- gaip/modtran.py
+++ gaip/modtran.py
@@ -241,11 +241,18 @@ def bilinear_interpolate(acqs, factors, coordinator, boxline, centreline,
 
     bands = [str(a.band_num) for a in acqs]
 
+    # Initialise the dict to store the locations of the bilinear outputs
+    bilinear_outputs = {}
+
     for band in bands:
         for factor in factors:
+            fname = output_fmt.format(factor=factor, band=band)
+            bilinear_outputs[(band, factor)] = pjoin(workpath, fname)
             args = [cmd, coordinator,
                     input_fmt.format(coord=coord, band=band),
                     boxline, centreline,
                     output_fmt.format(factor=factor, band=band)]
 
             subprocess.check_call(args, cwd=workpath)
+
+    return bilinear_outputs
