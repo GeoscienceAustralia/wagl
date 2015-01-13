d1e90ae40aaa021f30af2e794f0bfed618f52fd2
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-01-13 16:33:36 +1100

    Removed the test directory containing the pbs scripts for the old nbar system.

--- test/clean_nbar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export OUTPUT_ROOT=/short/v10/${USER}/tc/integration_tests/nbar
-
-rm -f test.nbar.*
-rm -rf ${OUTPUT_ROOT}/work/*
-rm -rf ${OUTPUT_ROOT}/output/*
--- test/clean_tc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export OUTPUT_ROOT=/short/v10/${USER}/tc/integration_tests/tc
-
-rm -f test.tc.*
-rm -rf ${OUTPUT_ROOT}/work/*
-rm -rf ${OUTPUT_ROOT}/output/*
