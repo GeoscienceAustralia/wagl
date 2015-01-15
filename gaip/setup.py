52ed3ab30fa5a2ec97abac49c1073f3a842b1eff
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-01-15 14:12:53 +1100

    Removed the unmiximage build. Wouldn't build through the setup.py. The unmiximage module is built via the Makefile.

--- gaip/setup.py
+++ gaip/setup.py
@@ -31,9 +31,5 @@ setup(name='gaip',
                              'geod2geo.f90',
                              'q_cal.f90',
                              'cal_angles.f90',
-                             'angle_all.f90']),
-          Extension(name='unmiximage',
-                    sources=['constants_NSWC.f90',
-                             'nnls.f90',
-                             'unmiximage.f90'])
+                             'angle_all.f90'])
       ])
