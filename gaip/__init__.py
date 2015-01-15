fc32fc296ecd413e561db658717e21c1b2feabe3
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-01-15 17:39:58 +1100

    Fixed saturation_bits import.

--- gaip/__init__.py
+++ gaip/__init__.py
@@ -15,7 +15,7 @@ from modtran import *
 from margins import *
 from constants import PQAConstants
 from pqa_result import PQAResult
-from saturation_masking import setSaturationBits
+from saturation_masking import set_saturation_bits
 from contiguity_masking import set_contiguity_bit
 from thermal_conversion import get_landsat_temperature
 from acca_cloud_masking import calc_acca_cloud_mask
