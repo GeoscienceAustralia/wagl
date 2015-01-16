4c91e8af1a253a49b122894ae40da177b6e995a9
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-01-16 13:42:17 +1100

    PEP8 fixes.

--- gaip/tc.py
+++ gaip/tc.py
@@ -250,7 +250,7 @@ def run_slope(acquisition, DEM, solar_zenith, satellite_view, solar_azimuth,
     dresy = y_res + 2
 
     # Get acquisition dimensions and add 1 pixel top, bottom, left & right
-    cols, rows = geobox.getShapeXY()
+    cols, rows = geobox.get_shape_xy()
     ncol = cols + 2
     nrow = rows + 2
 
