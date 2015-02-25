89ab01751a4145326c6f98184f7334f767ba3e8f
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-02-25 16:42:57 +1100

    Reworked the slope and self shadow workflow. They're now separate and work using a configurable tiling scheme.

--- gaip/self_shadow.py
+++ gaip/self_shadow.py
@@ -2,6 +2,11 @@
 Shadow Calculations
 -------------------
 """
+
+import gdal
+import numpy
+import rasterio
+
 from gaip import ImageMargins
 from gaip import setup_spheroid
 from gaip import read_img
@@ -111,3 +116,69 @@ def write_header_slope_file(file_name, margins, geobox):
         output.write("{resy} {resx}\n".format(resx=res[0], resy=res[1]))
         output.write("{yorigin} {xorigin}\n".format(xorigin=origin[0],
                                                     yorigin=origin[1]))
+
+
+def self_shadow(incident_fname, exiting_fname, self_shadow_out_fname,
+                header_slope_fname=None):
+    """
+    
+    """
+
+    with rasterio.open(incident_fname) as inc_ds,\
+        rasterio.open(exiting_fname) as exi_ds:
+
+        # Retrieve a geobox and image info
+        geobox = GriddedGeoBox.from_rio_dataset(inc_ds)
+        cols, rows = geobox.get_shape_xy()
+        prj = geobox.crs.ExportToWkt()
+        geoT = geobox.affine.to_gdal()
+
+        # Initialise the output file
+        drv = gdal.GetDriverByName("ENVI")
+        out_dtype = gdal.GDT_Byte
+        nbands = 1
+        outds = drv.Create(self_shadow_out_fname, cols, rows, nbands,
+                           out_dtype)
+        outds.SetProjection(prj)
+        outds.SetGeoTransform(geoT)
+        outband = outds.GetRasterBand(1)
+
+        # Initialise the tiling scheme for processing
+        if X_TILE is None:
+            X_TILE = cols
+        if Y_TILE is None:
+            Y_TILE = 1
+        tiles = tiling.generate_tiles(cols, rows, X_TILE, Y_TILE,
+                                      Generator=False)
+
+        # Loop over each tile
+        for tile in tiles:
+            # Row and column start and end locations
+            ystart = tile[0][0]
+            xstart = tile[1][0]
+            yend = tile[0][1]
+            xend = tile[1[0]
+
+            # Tile size
+            ysize = yend - ystart
+            xsize = xend - xstart
+
+            # Read the data for the current tile
+            inc = numpy.radians(inc_ds.read_band(1, window=tile, masked=False))
+            exi = numpy.radians(exi_ds.read_band(1, window=tile, masked=False))
+
+            # Process the tile
+            mask = numpy.ones((rows, cols), dtype='uint8')
+            mask[inc <= 0.0] = 0
+            mask[exi <= 0.0] = 0
+
+            # Write the current tile to disk
+            outband.WriteArray(mask, xstart, ystart)
+            outband.FlushCache()
+
+        # Close the file
+        outband = None
+        outds = None
+
+    if header_slope_fname:
+        write_header_slope_file(header_slope_fname, pixel_buf, geobox)
