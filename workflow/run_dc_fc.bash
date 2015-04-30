ccd4f2da7eb087f9a277f3c496767fcf105ef688
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-04-30 13:13:59 +1000

    Reverted back to gdal/1.9.2

--- workflow/run_dc_fc.bash
+++ workflow/run_dc_fc.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P v10
-#PBS -q normal
-#PBS -l walltime=00:50:00,mem=64GB,ncpus=32
+#PBS -q express
+#PBS -l walltime=00:50:00,mem=32GB,ncpus=16
 #PBS -l wd
 #PBS -me
 #PBS -M joshua.sixsmith@ga.gov.au
@@ -9,9 +9,9 @@
 module load python/2.7.6
 
 module use /projects/u46/opt/modules/modulefiles
-module load gdal/1.10.1
+module load gdal
 module load gaip
 module load agdc-api
 
 
-mpirun -n 32 python dc_fc.py
+mpirun -n 16 python dc_fc.py
