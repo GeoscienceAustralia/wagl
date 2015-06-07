ff40bf6e2fcdffc272ac5f573a52e364d807a94a
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-06-07 13:50:15 +1000

    Converted to scene processing of FC toPBSDSH.

--- workflow/fc.py
+++ workflow/fc.py
@@ -1,6 +1,5 @@
 #!/bin/env python
 
-import luigi.contrib.mpi as mpi
 import luigi
 import argparse
 import os
@@ -99,31 +98,63 @@ def fc_name_from_nbar(nbar_fname):
     """
     return nbar_fname.replace('NBAR', 'FC')
 
+def scatter(iterable, P=1, p=1):
+    """
+    Scatter an iterator across `P` processors where `p` is the index
+    of the current processor. This partitions the work evenly across
+    processors.
+    """
+    import itertools
+    return itertools.islice(iterable, p-1, None, P)
+ 
+ 
+def main(nbar_path, outpath, nnodes=1, nodenum=1):
+    nbar_files = sorted([pjoin(nbar_path, f) for f in os.listdir(nbar_path) if
+                        '_NBAR_' in f])
+    nbar_files = [basename(f) for f in scatter(l1t_files, nnodes, nodenum)]
+    ncpus = int(os.getenv('PBS_NCPUS', '1'))
+    tasks = []
+    for nbar_file in nbar_files:
+        nbar_dataset_path = pjoin(nbar_path, nbar_file)
+        fc_dataset_path = os.path.join(outpath, fc_name_from_nbar(nbar_file))
+        tasks.append(FractionalCoverTask(nbar_dataset_path,
+                                         fc_dataset_path))
+
+    luigi.build(tasks, local_scheduler=True, workers=16)
+
+
 if __name__ == '__main__':
     # command line arguments
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--nbar_path", help="path to directory containing NBAR datasets", \
-        required=True, type=lambda x: is_valid_directory(parser, x))
-    parser.add_argument("--out_path", help="path to directory where FC dataset is to be written", \
-        required=True, type=lambda x: is_valid_directory(parser, x))
-    parser.add_argument("--log_path", help="path to directory where where log files will be written", \
-        default='.', type=lambda x: is_valid_directory(parser, x))
-    parser.add_argument("--debug", help="selects more detail logging (default is INFO)", \
-        default=False, action='store_true')
+    parser.add_argument("--nbar_path",
+                        help="path to directory containing NBAR datasets",
+                        required=True,
+                        type=lambda x: is_valid_directory(parser, x))
+    parser.add_argument("--out_path",
+                        help=("path to directory where FC dataset is to "
+                              "be written"), required=True,
+                        type=lambda x: is_valid_directory(parser, x))
+    parser.add_argument("--log_path",
+                        help=("path to directory where where log files will "
+                              "be written"), default='.',
+                        type=lambda x: is_valid_directory(parser, x))
+    parser.add_argument("--debug",
+                        help="selects more detail logging (default is INFO)",
+                        default=False, action='store_true')
 
     args = parser.parse_args()
 
     # setup logging
+    fmt = '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s'
     
     logfile = "run_fc_{}_{}.log".format(os.uname()[1], os.getpid())
     logfile = os.path.join(args.log_path, logfile)
     logging_level = logging.INFO
     if args.debug:
         logging_level = logging.DEBUG
-    logging.basicConfig(filename=logfile, level=logging_level, \
-        format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
-        datefmt='%H:%M:%S')
+    logging.basicConfig(filename=logfile, level=logging_level,
+                        format=fmt, datefmt='%H:%M:%S')
     logging.info("fc.py started")
 
 
@@ -131,16 +162,8 @@ if __name__ == '__main__':
     logging.info('out_path={}'.format(args.out_path))
     logging.info('log_path={}'.format(args.log_path))
 
-    # create the task list based on L1T files to process
 
-    tasks = []
-    for nbar_file in [f for f in os.listdir(args.nbar_path) if '_NBAR_' in f]:
-        nbar_dataset_path = os.path.join(args.nbar_path, nbar_file)
-        fc_dataset_path = os.path.join(args.out_path,
-                                       fc_name_from_nbar(nbar_file))
-
-        tasks.append(FractionalCoverTask(nbar_dataset_path, fc_dataset_path))
-        
-        print nbar_dataset_path
-
-    mpi.run(tasks)
+    size = int(os.getenv('PBS_NNODES', '1'))
+    rank = int(os.getenv('PBS_VNODENUM', '1'))
+    main(args.l1t_path, args.nbar_path, args.land_sea_path, args.out_path,
+         size, rank)
