421db8f73e32a15d34e62a6876c39a2c18c98e3b
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-06-29 13:36:58 +1000

    Added a commandline option to include a user config file.

--- workflow/query_agdc.py
+++ workflow/query_agdc.py
@@ -5,13 +5,12 @@ from os.path import join as pjoin, dirname
 import cPickle as pickle
 import subprocess
 from datetime import date
+import argparse
 
 from datacube.api.model import DatasetType, Satellite
 from datacube.api.query import list_tiles_as_list
 from datacube.config import Config
 
-CONFIG = luigi.configuration.get_config()
-CONFIG.add_config_path(pjoin(dirname(__file__), 'fc.cfg'))
 
 PBS_DSH = (
 """#!/bin/bash
@@ -27,7 +26,7 @@ NNODES={nnodes}
 
 for i in $(seq 1 $NNODES); do
    pbsdsh -n $((16 *$i)) -- bash -l -c "{modules} python {pyfile} \
-   --tile $[$i - 1]" &
+   --tile $[$i - 1]{config}" &
 done;
 wait
 """)
@@ -50,7 +49,7 @@ def create_tiles(array_size, tile_size=25):
     return idx_tiles
 
 
-def query(output_path):
+def query(output_path, cfg=None):
     """
     Queries the database based on the parameters given by the
     config file (default is fc.cfg).
@@ -120,10 +119,14 @@ def query(output_path):
     walltime = CONFIG.get('pbs', 'walltime')
     email = CONFIG.get('pbs', 'email')
     py_file = pjoin(dirname(__file__), 'agdc_fc.py')
+    if cfg is None:
+        cfg = ''
+    else:
+        cfg = ' --cfg {}'.format(cfg)
     pbs_job = PBS_DSH.format(project=project, queue=queue,
                              walltime=walltime, ncpus=ncpus, mem=mem,
                              email=email, nnodes=nnodes,
-                             modules=modules, pyfile=py_file)
+                             modules=modules, pyfile=py_file, config=cfg)
 
     # Output the shell script to disk
     pbs_fname = pjoin(output_path, CONFIG.get('pbs', 'pbs_filename'))
@@ -134,5 +137,23 @@ def query(output_path):
     subprocess.check_call(['qsub', pbs_fname])
 
 if __name__ == '__main__':
+    desc = ("The database query mechanism used for processing "
+            "NBAR to Fractional Cover.")
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--cfg',
+                       help='Path to a user defined configuration file.')
+
+    parsed_args = parser.parse_args()
+    cfg = parsed_args.cfg
+
+    # Setup the config file
+    global CONFIG
+    if cfg is None:
+        CONFIG = luigi.configuration.get_config()
+        CONFIG.add_config_path(pjoin(dirname(__file__), 'fc.cfg'))
+    else:
+        CONFIG = luigi.configuration.get_config()
+        CONFIG.add_config_path(cfg)
+
     out_dir = CONFIG.get('agdc', 'output_directory')
-    query(out_dir)
+    query(out_dir, cfg)
