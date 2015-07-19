c0f4f00e0dee64e721bab2faa2538dd5ee45bc29
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-07-19 14:57:47 +1000

    Added a parameterised test case for individual files.

--- gaip/tests/unittesting_tools.py
+++ gaip/tests/unittesting_tools.py
@@ -75,6 +75,69 @@ class ParameterisedTestCase(unittest.TestCase):
         return suite
 
 
+class ParameterisedTestCaseFiles(unittest.TestCase):
+    """ 
+    TestCase classes that want to be parameterised should
+    inherit from this class.
+
+    Source for this code taken from:
+    http://eli.thegreenplace.net/2011/08/02/python-unit-testing-parametrized-test-cases/
+
+    Modified to suit our given parameters.
+    """
+    def __init__(self, methodName='runTest', reference_fname=None,
+                 test_fname=None, decimal_precision=4, integer_precision=1):
+        super(ParameterisedTestCase, self).__init__(methodName)
+
+        # Reference and Testing directories
+        self.reference_fname = reference_fname
+        self.test_fname = test_fname
+
+        # Allowable numerical precision
+        self.decimal_precision = decimal_precision
+        self.integer_precision = integer_precision
+
+
+    @staticmethod
+    def parameterise(testcase_klass, reference_fname=None, test_fname=None,
+                    decimal_precision=4, integer_precision=1):
+        """
+        Create a suite containing all tests taken from the given
+        subclass, passing them the parameters 'reference_fname,
+        test_fname, decimal_precision, integer_precision'.
+
+        :param testcase_klass:
+            A unittest.TestCase Class
+
+        :param reference_fname:
+            A full file pathname oo the file containing the
+            reference data.
+
+        :param test_fname:
+            A full file pathname to the file containing the
+            test data.
+
+        :param decimal_precision:
+            The decimal precision to be used during array comparison.
+            Default is 4, i.e. values must be correct up to 4 d.p. in
+            order to Pass.
+
+        :param integer_precision:
+            The intger precision to be used during array comparison.
+            Default is 1, i.e. values must be correct within 1 integer
+            in order to Pass.
+        """
+        testloader = unittest.TestLoader()
+        testnames = testloader.getTestCaseNames(testcase_klass)
+        suite = unittest.TestSuite()
+        for name in testnames:
+            suite.addTest(testcase_klass(name, reference_fname=reference_fname,
+                          test_fname=test_fname,
+                          decimal_precision=decimal_precision,
+                          integer_precision=integer_precision))
+        return suite
+
+
 def createTestImage(dimensions=(1000,1000), geotransform=None,
         projection=None, resolution=(25.0,25.0), dtype='uint8'):
     """
