23807a9ede46a4e22d47ce517a8b27d8e18a2bce
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-07-23 09:51:38 +1000

    Added more functionality to the markdown reporting.

--- gaip/tests/unittesting_tools.py
+++ gaip/tests/unittesting_tools.py
@@ -86,21 +86,23 @@ class ParameterisedTestCaseFiles(unittest.TestCase):
     """
 
     def __init__(self, methodName='runTest', reference_fname=None,
-                 test_fname=None, decimal_precision=4, integer_precision=1):
+                 test_fname=None, tolerance=3, output_directory=''):
         super(ParameterisedTestCaseFiles, self).__init__(methodName)
 
         # Reference and Testing directories
         self.reference_fname = reference_fname
         self.test_fname = test_fname
 
-        # Allowable numerical precision
-        self.decimal_precision = decimal_precision
-        self.integer_precision = integer_precision
+        # Allowable tollerance
+        self.tolerance = tolerance
+
+        # Output directory
+        self.output_directory = output_directory
 
 
     @staticmethod
     def parameterise(testcase_klass, reference_fname=None, test_fname=None,
-                    decimal_precision=4, integer_precision=1):
+                     tolerance=3, output_directory=''):
         """
         Create a suite containing all tests taken from the given
         subclass, passing them the parameters 'reference_fname,
@@ -117,15 +119,12 @@ class ParameterisedTestCaseFiles(unittest.TestCase):
             A full file pathname to the file containing the
             test data.
 
-        :param decimal_precision:
-            The decimal precision to be used during array comparison.
-            Default is 4, i.e. values must be correct up to 4 d.p. in
+        :param tolerance:
+            Default is 3, i.e. tolerance is 97% must be the same in
             order to Pass.
 
-        :param integer_precision:
-            The intger precision to be used during array comparison.
-            Default is 1, i.e. values must be correct within 1 integer
-            in order to Pass.
+        :param output_directory:
+            The output directory to contain any results.
         """
         testloader = unittest.TestLoader()
         testnames = testloader.getTestCaseNames(testcase_klass)
@@ -133,8 +132,8 @@ class ParameterisedTestCaseFiles(unittest.TestCase):
         for name in testnames:
             suite.addTest(testcase_klass(name, reference_fname=reference_fname,
                           test_fname=test_fname,
-                          decimal_precision=decimal_precision,
-                          integer_precision=integer_precision))
+                          tolerance=tolerance,
+                          output_directory=output_directory))
         return suite
 
 
