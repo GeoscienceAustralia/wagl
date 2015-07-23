ddafc22d2c2f9e00a84b0f5ed7da7ae4afa562b8
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-07-23 22:17:29 +1000

    Added a new param to the unittesting tools.

--- gaip/tests/unittesting_tools.py
+++ gaip/tests/unittesting_tools.py
@@ -21,7 +21,8 @@ class ParameterisedTestCase(unittest.TestCase):
     Modified to suit our given parameters.
     """
     def __init__(self, methodName='runTest', reference_dir=None, test_dir=None,
-                 decimal_precision=4, integer_precision=1):
+                 decimal_precision=4, integer_precision=1,
+                 output_directory=''):
         super(ParameterisedTestCase, self).__init__(methodName)
 
         # Reference and Testing directories
@@ -32,10 +33,14 @@ class ParameterisedTestCase(unittest.TestCase):
         self.decimal_precision = decimal_precision
         self.integer_precision = integer_precision
 
+        # Output directory
+        self.output_directory = output_directory
+
 
     @staticmethod
     def parameterise(testcase_klass, reference_dir=None, test_dir=None,
-                    decimal_precision=4, integer_precision=1):
+                     decimal_precision=4, integer_precision=1,
+                     output_directory=''):
         """
         Create a suite containing all tests taken from the given
         subclass, passing them the parameters 'reference_dir,
@@ -61,6 +66,9 @@ class ParameterisedTestCase(unittest.TestCase):
             The intger precision to be used during array comparison.
             Default is 1, i.e. values must be correct within 1 integer
             in order to Pass.
+
+        :param output_directory:
+            The output directory to contain any results.
         """
         testloader = unittest.TestLoader()
         testnames = testloader.getTestCaseNames(testcase_klass)
@@ -69,7 +77,8 @@ class ParameterisedTestCase(unittest.TestCase):
             suite.addTest(testcase_klass(name, reference_dir=reference_dir,
                           test_dir=test_dir,
                           decimal_precision=decimal_precision,
-                          integer_precision=integer_precision))
+                          integer_precision=integer_precision,
+                          output_directory=output_directory))
         return suite
 
 
