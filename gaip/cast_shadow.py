99c6dcc7e770a6d715729bfb18d27d67d89bdaa5
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-02-13 17:37:53 +1100

    Re-worked to utilise hdf5 for I/O and metadata.

--- gaip/cast_shadow.py
+++ gaip/cast_shadow.py
@@ -1,25 +1,61 @@
+#!/usr/bin/env python
+
 """
-Shadow Casting
---------------
+Cast shadow calculation
+-----------------------
 """
 
-import rasterio
+import h5py
 from gaip import ImageMargins
 from gaip import setup_spheroid
 from gaip import run_castshadow
-from gaip import write_img
+from gaip import dataset_compression_kwargs
+from gaip import attach_image_attributes
 
 
-def calculate_cast_shadow(acquisition, dsm_fname, margins, block_height,
-                          block_width, view_angle_fname, azimuth_angle_fname,
-                          outfname):
+def _calculate_cast_shadow_wrapper(acquisition, dsm_fname, margins,
+                                   block_height, block_width,
+                                   satellite_solar_angles_fname, out_fname,
+                                   compression='lzf', solar_source=True):
+    """
+    A private wrapper for dealing with the internal custom workings of the
+    NBAR workflow.
+    """
+    with h5py.File(dsm_fname, 'r') as dsm_src,\
+        h5py.File(satellite_solar_angles_fname, 'r') as sat_sol:
+
+        dsm_dset = dsm_src['dsm-smoothed']
+
+        if solar_source:
+            view_name = 'solar-zenith-angles'
+            azimuth_name = 'solar-azimuth-angles'
+        else:
+            view_name = 'satellite-view-angles'
+            azimuth_name = 'satellite-azimuth-angles'
+
+        view_dset = sat_sol[view_name]
+        azi_dset = sat_sol[azimuth_name]
+
+    fid = calculate_cast_shadow(acquisition, dsm_dset, margins, block_height,
+                                block_width, view_dset, azi_dset, out_fname,
+                                compression, solar_source)
+
+    fid.close()
+    return
+
+
+def calculate_cast_shadow(acquisition, dsm_dataset, margins, block_height,
+                          block_width, view_angle_dataset,
+                          azimuth_angle_dataset, out_fname=None,
+                          compression='lzf', solar_source=True):
     """
     :param acquisition:
         An instance of an acquisition object.
 
-    :param dsm_fname:
-        A string containing the full file path name to the Digital
-        Surface Model to be used in deriving the surface angles.
+    :param dsm_dataset:
+        A `NumPy` or `NumPy` like dataset that allows indexing
+        and returns a `NumPy` dataset containing the Digital Surface
+        Model data when index/sliced.
 
     :param margins:
         An object with members top, bottom, left and right giving the
@@ -34,22 +70,44 @@ def calculate_cast_shadow(acquisition, dsm_fname, margins, block_height,
         The width (rows) of the window/submatrix used in the cast
         shadow algorithm.
 
-    :param view_angle_fname:
-        A string containing the full file path name to the view
-        angle image. i.e. solar zenith angle or the the view angle to
-        the satellite.
+    :param view_angle_dataset:
+        A `NumPy` or `NumPy` like dataset that allows indexing
+        and returns a `NumPy` dataset containing the solar zenith
+        or the satellite view angles when index/sliced.
 
-    :param azimuth_angle_fname:
-        A string containing the full file path name to the azimuth
-        angle image. i.e. Solar azimuth angle or the satellite azimuth
-        angle.
+    :param azimuth_angle_dataset:
+        A `NumPy` or `NumPy` like dataset that allows indexing
+        and returns a `NumPy` dataset containing the solar azimuth
+        or the satellite azimuth angles when index/sliced.
 
-    :param outfname:
-        A string containing the full file path name to be used for
-        creating the output image.
+    :param out_fname:
+        If set to None (default) then the results will be returned
+        as an in-memory hdf5 file, i.e. the `core` driver.
+        Otherwise it should be a string containing the full file path
+        name to a writeable location on disk in which to save the HDF5
+        file.
+
+        The dataset names will be as follows:
+
+        * cast_shadow_{source} where source is either the sun or satellite
+
+    :param compression:
+        The compression filter to use. Default is 'lzf'.
+        Options include:
+
+        * 'lzf' (Default)
+        * 'lz4'
+        * 'mafisc'
+        * An integer [1-9] (Deflate/gzip)
+
+    :param solar_source:
+        A `bool` indicating whether or not the source for the line
+        of sight comes from the sun (True; Default), or False
+        indicating the satellite.
 
     :return:
-        None. The result is written to disk.
+        An opened `h5py.File` object, that is either in-memory using the
+        `core` driver, or on disk.
     """
     # Setup the geobox
     geobox = acquisition.gridded_geo_box()
@@ -59,12 +117,9 @@ def calculate_cast_shadow(acquisition, dsm_fname, margins, block_height,
     spheroid = setup_spheroid(geobox.crs.ExportToWkt())
 
     # Read the dsm and angle arrays into memory
-    with rasterio.open(dsm_fname) as ds:
-        dsm = ds.read(1)
-    with rasterio.open(view_angle_fname) as ds:
-        view_angle = ds.read(1)
-    with rasterio.open(azimuth_angle_fname) as ds:
-        azimuth_angle = ds.read(1)
+    dsm = dsm_dataset[:]
+    view_angle = view_angle_dataset[:]
+    azimuth_angle = azimuth_angle_dataset[:]
 
     # Define Top, Bottom, Left, Right pixel margins
     pixel_buf = ImageMargins(margins)
@@ -73,6 +128,31 @@ def calculate_cast_shadow(acquisition, dsm_fname, margins, block_height,
     mask = run_castshadow(acquisition, dsm, view_angle, azimuth_angle,
                           pixel_buf, block_height, block_width, spheroid)
 
-    # Output the result to disk
-    write_img(mask, outfname, fmt='GTiff', geobox=geobox, nodata=-999,
-              compress='deflate', options={'zlevel': 1})
+    source_dir = 'sun' if solar_source else 'satellite'
+
+    # Initialise the output files
+    if out_fname is None:
+        fid = h5py.File('cast_shadow_{}.h5'.format(source_dir), driver='core',
+                        backing_store=False)
+    else:
+        fid = h5py.File(out_fname, 'w')
+
+    kwargs = dataset_compression_kwargs(compression=compression,
+                                        chunks=(1, geobox.x_size()))
+    no_data = -999
+    kwargs['fillvalue'] = no_data
+    kwargs['no_data_value'] = no_data
+
+    out_dset = fid.create_dataset('cast_shadow_{}'.format(source_dir),
+                                  data=mask, **kwargs)
+
+    # attach some attributes to the image datasets
+    attrs = {'crs_wkt': geobox.crs.ExportToWkt(),
+             'geotransform': geobox.affine.to_gdal()}
+    desc = ("The cast shadow mask determined using the {} "
+            "as the source direction.").format(source_dir)
+    attrs['Description'] = desc
+    attach_image_attributes(out_dset, attrs)
+
+    fid.flush()
+    return fid
