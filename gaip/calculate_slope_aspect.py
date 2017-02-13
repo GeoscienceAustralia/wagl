d30ad38d033c90608f9a6cd60158e42092247cc9
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-02-13 17:00:12 +1100

    Re-worked to utilise hdf5 for I/O and metadata.

--- gaip/calculate_slope_aspect.py
+++ gaip/calculate_slope_aspect.py
@@ -1,65 +1,79 @@
-#!/usr/bin/env
+#!/usr/bin/env python
+
+"""
+Calculates the slope and aspect for a given elevation dataset.
+"""
 
 import numpy
-import rasterio
+import h5py
 
 from gaip import as_array
 from gaip import ImageMargins
 from gaip import setup_spheroid
 from gaip import slope_aspect
-from gaip import write_img
+from gaip import dataset_compression_kwargs
+from gaip import attach_image_attributes
 
 
-def write_header_slope_file(file_name, margins, geobox):
-    """Write the header slope file."""
-    with open(file_name, 'w') as output:
-        # get dimensions, resolution and pixel origin
-        rows, cols = geobox.shape
-        res = geobox.pixelsize
-        origin = geobox.origin
+def _slope_aspect_arrays_wrapper(acquisition, dsm_fname, margins, out_fname,
+                                 compression='lzf'):
+    """
+    A private wrapper for dealing with the internal custom workings of the
+    NBAR workflow.
+    """
+    with h5py.File(dsm_fname, 'r') as src:
+        dsm_dset = src['dsm-smoothed']
 
-        # Now output the details
-        output.write("{nr} {nc}\n".format(nr=rows, nc=cols))
-        output.write("{0} {1}\n{2} {3}\n".format(margins.left, margins.right,
-                                                 margins.top, margins.bottom))
-        output.write("{resy} {resx}\n".format(resx=res[0], resy=res[1]))
-        output.write("{yorigin} {xorigin}\n".format(xorigin=origin[0],
-                                                    yorigin=origin[1]))
+        fid = slope_aspect_arrays(acquisition, dsm_dset, margins, out_fname,
+                                  compression)
+
+    fid.close()
+    return
 
 
-def slope_aspect_arrays(acquisition, dsm_fname, margins, slope_out_fname,
-                        aspect_out_fname, header_slope_fname=None):
+def slope_aspect_arrays(acquisition, dsm_dataset, margins, out_fname=None,
+                        compression='lzf'):
     """
     Calculates slope and aspect.
 
     :param acquisition:
         An instance of an acquisition object.
 
-    :param dsm_fname:
-        A string containing the full file path name to the Digital
-        Surface Model to be used in deriving the surface angles.
+    :param dsm_dataset:
+        A `NumPy` or `NumPy` like dataset that allows indexing
+        and returns a `NumPy` dataset containing the Digital Surface
+        Model data when index/sliced.
 
     :param margins:
         An object with members top, bottom, left and right giving the
         size of the margins (in pixels) which have been added to the
         corresponding sides of dsm.
 
-    :param slope_out_fname:
-        A string containing the full file path name to be used for
-        writing the slope image on disk.
+    :param out_fname:
+        If set to None (default) then the results will be returned
+        as an in-memory hdf5 file, i.e. the `core` driver.
+        Otherwise it should be a string containing the full file path
+        name to a writeable location on disk in which to save the HDF5
+        file.
 
-    :param aspect_out_fname:
-        A string containing the full file path name to be used for
-        writing the aspect image on disk.
+        The dataset names will be as follows:
 
-    :param header_slope_fname:
-        A string containing the full file path name to be used for
-        writing the header slope text file to disk.
+        * slope
+        * aspect
+
+    :param compression:
+        The compression filter to use. Default is 'lzf'.
+        Options include:
+
+        * 'lzf' (Default)
+        * 'lz4'
+        * 'mafisc'
+        * An integer [1-9] (Deflate/gzip)
 
     :return:
-        None. Outputs are written to disk.
+        An opened `h5py.File` object, that is either in-memory using the
+        `core` driver, or on disk.
     """
-
     # Setup the geobox
     geobox = acquisition.gridded_geo_box()
 
@@ -76,39 +90,66 @@ def slope_aspect_arrays(acquisition, dsm_fname, margins, slope_out_fname,
     # Get the x and y pixel sizes
     _, y_origin = geobox.origin
     x_res, y_res = geobox.pixelsize
-    dresx = x_res
-    dresy = y_res
 
     # Get acquisition dimensions and add 1 pixel top, bottom, left & right
     cols, rows = geobox.get_shape_xy()
     ncol = cols + 2
     nrow = rows + 2
 
+    # TODO: check that the index is correct
     # Define the index to read the DEM subset
-    idx = ((pixel_margin.top - 1, -(pixel_margin.bottom - 1)),
-           (pixel_margin.left - 1, -(pixel_margin.right - 1)))
+    ystart, ystop = (pixel_margin.top - 1, -(pixel_margin.bottom - 1))
+    xstart, xstop = (pixel_margin.left - 1, -(pixel_margin.right - 1))
+    idx = (slice(ystart, ystop), slice(xstart, xstop))
 
-    with rasterio.open(dsm_fname) as dsm_ds:
-        dsm_subset = as_array(dsm_ds.read(1, window=idx, masked=False),
-                              dtype=numpy.float32, transpose=True)
+    dsm_subset = as_array(dsm_dataset[idx], dtype=numpy.float32,
+                          transpose=True)
 
     # Define an array of latitudes
     # This will be ignored if is_utm == True
-    alat = numpy.array([y_origin - i * dresy for i in range(-1, nrow - 1)],
+    alat = numpy.array([y_origin - i * y_res for i in range(-1, nrow - 1)],
                        dtype=numpy.float64)  # yes, I did mean float64.
+
+    # Output the reprojected result
+    # Initialise the output files
+    if out_fname is None:
+        fid = h5py.File('slope-aspect.h5', driver='core',
+                        backing_store=False)
+    else:
+        fid = h5py.File(out_fname, 'w')
+
+    # metadata for calculation
+    group = fid.create_group('parameters')
+    group.attrs['dsm_index'] = ((ystart, ystop), (xstart, xstop))
+    group.attrs['calculation_pixel_buffer'] = '1 pixel'
+
+    kwargs = dataset_compression_kwargs(compression=compression,
+                                        chunks=(1, geobox.x_size()))
+    no_data = -999
+    kwargs['fillvalue'] = no_data
+    kwargs['no_data_value'] = no_data
 
     # Define the output arrays. These will be transposed upon input
     slope = numpy.zeros((rows, cols), dtype='float32')
     aspect = numpy.zeros((rows, cols), dtype='float32')
 
-    slope_aspect(ncol, nrow, cols, rows, dresx, dresy, spheroid, alat, is_utm,
-                 dsm_subset, slope.transpose(), aspect.transpose())
+    slope_aspect(ncol, nrow, cols, rows, x_res, y_res, spheroid, alat, is_utm,
+                 dsm_subset.transpose(), slope.transpose(), aspect.transpose())
 
-    # Output the results
-    write_img(slope, slope_out_fname, fmt='GTiff', geobox=geobox, nodata=-999,
-              compress='deflate', options={'zlevel': 1})
-    write_img(aspect, aspect_out_fname, fmt='GTiff', geobox=geobox,
-              nodata=-999, compress='deflate', options={'zlevel': 1})
+    # output datasets
+    slope_dset = fid.create_dataset('slope', data=slope, **kwargs)
+    aspect_dset = fid.create_dataset('aspect', data=aspect, **kwargs)
 
-    if header_slope_fname:
-        write_header_slope_file(header_slope_fname, pixel_margin, geobox)
+    # attach some attributes to the image datasets
+    attrs = {'crs_wkt': geobox.crs.ExportToWkt(),
+             'geotransform': geobox.affine.to_gdal()}
+    desc = "The slope derived from the input elevation model."
+    attrs['Description'] = desc
+    attach_image_attributes(slope_dset, attrs)
+
+    desc = "The aspect derived from the input elevation model."
+    attrs['Description'] = desc
+    attach_image_attributes(aspect_dset, attrs)
+
+    fid.flush()
+    return fid
