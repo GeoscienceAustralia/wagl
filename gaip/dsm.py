d5ecc342bf8f34aab72065d8b371d338a5e98810
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-02-13 15:42:01 +1100

    Re-worked to utilise hdf5 for I/O and metadata.

--- gaip/dsm.py
+++ gaip/dsm.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python
 """
-Digital Surface Model Data
+Digital Surface Model Data extraction and smoothing.
 """
 
+import h5py
+from rasterio.warp import RESAMPLING
 from gaip import ImageMargins
 from gaip import filter_dsm
 from gaip import GriddedGeoBox
 from gaip import reproject_file_to_array
-from gaip import write_img
-from rasterio.warp import RESAMPLING
+from gaip import dataset_compression_kwargs
+from gaip import attach_image_attributes
 
 
-def get_dsm(acquisition, national_dsm, margins, fname_subset, fname_smoothed):
+def get_dsm(acquisition, national_dsm, margins, out_fname=None,
+            compression='lzf'):
     """
     Given an acquisition and a national Digitial Surface Model,
     extract a subset from the DSM based on the acquisition extents
@@ -32,27 +36,41 @@ def get_dsm(acquisition, national_dsm, margins, fname_subset, fname_smoothed):
         bottom, left and right will be added to the acquisition
         margin/border.
 
-    :param fname_subset:
-        A string containing the full filepath name to a location on
-        disk that will contain the subsetted dsm.
+    :param out_fname:
+        If set to None (default) then the results will be returned
+        as an in-memory hdf5 file, i.e. the `core` driver.
+        Otherwise it should be a string containing the full file path
+        name to a writeable location on disk in which to save the HDF5
+        file.
 
-    :param fname_smoothed:
-        A string containing the full filepath name to a location on
-        disk that will contain the subsetted and smoothed dsm.
+        The dataset names will be as follows:
+
+        * dsm
+        * dsm-smoothed
+
+    :param compression:
+        The compression filter to use. Default is 'lzf'.
+        Options include:
+
+        * 'lzf' (Default)
+        * 'lz4'
+        * 'mafisc'
+        * An integer [1-9] (Deflate/gzip)
 
     :return:
-        None, the results will be written to disk.
+        An opened `h5py.File` object, that is either in-memory using the
+        `core` driver, or on disk.
     """
-
     # Use the 1st acquisition to setup the geobox
     geobox = acquisition.gridded_geo_box()
+    shape = geobox.get_shape_yx()
 
     # Define Top, Bottom, Left, Right pixel margins
     pixel_buf = ImageMargins(margins)
 
     # Get the dimensions and geobox of the new image
-    dem_cols = geobox.get_shape_xy()[0] + pixel_buf.left + pixel_buf.right
-    dem_rows = geobox.get_shape_xy()[1] + pixel_buf.top + pixel_buf.bottom
+    dem_cols = shape[1] + pixel_buf.left + pixel_buf.right
+    dem_rows = shape[0] + pixel_buf.top + pixel_buf.bottom
     dem_shape = (dem_rows, dem_cols)
     dem_origin = geobox.convert_coordinates((0 - pixel_buf.left,
                                              0 - pixel_buf.top))
@@ -65,12 +83,33 @@ def get_dsm(acquisition, national_dsm, margins, fname_subset, fname_smoothed):
                                        resampling=RESAMPLING.bilinear)
 
     # Output the reprojected result
-    write_img(dsm_data, fname_subset, fmt='GTiff', geobox=dem_geobox,
-              compress='deflate', options={'zlevel': 1})
+    # Initialise the output files
+    if out_fname is None:
+        fid = h5py.File('dsm-extraction.h5', driver='core',
+                        backing_store=False)
+    else:
+        fid = h5py.File(out_fname, 'w')
+
+    kwargs = dataset_compression_kwargs(compression=compression,
+                                        chunks=(1, geobox.x_size()))
+
+    # output datasets
+    out_dset = fid.create_dataset('dsm', data=dsm_data, **kwargs)
+
+    # attach some attributes to the image datasets
+    attrs = {'crs_wkt': geobox.crs.ExportToWkt(),
+             'geotransform': dem_geobox.affine.to_gdal()}
+    desc = "A subset of a Digital Surface Model."
+    attrs['Description'] = desc
+    attach_image_attributes(out_dset, attrs)
 
     # Smooth the DSM
     dsm_data = filter_dsm(dsm_data)
+    out_sm_dset = fid.create_dataset('dsm-smoothed', data=dsm_data, **kwargs)
+    desc = ("A subset of a Digital Surface Model smoothed with a gaussian "
+            "kernel.")
+    attrs['Description'] = desc
+    attach_image_attributes(out_sm_dset, attrs)
 
-    # Output the smoothed DSM
-    write_img(dsm_data, fname_smoothed, fmt='GTiff', geobox=dem_geobox,
-              compress='deflate', options={'zlevel': 1})
+    fid.flush()
+    return fid
