7b0bda697de402783f5a55452a9efae341aa023e
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-02-24 22:48:19 +1100

    Changed wrapper func for accumulation caluculations

--- gaip/modtran.py
+++ gaip/modtran.py
@@ -602,24 +602,26 @@ def read_modtran_flux(fname):
     return flux_data, altitude
 
 
-def _calculate_solar_radiation(flux_fname, response_fname, transmittance,
-                               out_fname, compression='lzf'):
+def _calculate_solar_radiation(flux_fnames, response_fname, out_fname,
+                               compression='lzf'):
     """
     A private wrapper for dealing with the internal custom workings of the
     NBAR workflow.
     """
-    df = calculate_solar_radiation(flux_fname, response_fname, transmittance)
-    dset_name = splitext(basename(out_fname))[0]
-    coordinate, _, albedo = dset_name.split('-')
-
+    description = ("Accumulated solar irradiation for point {} "
+                   "and albedo {}.")
     with h5py.File(out_fname, 'w') as fid:
+        for key in flux_fnames:
+            point, albedo = key
+            dset_name = 'point-{}-albedo-{}'.format(point, albedo)
+            attrs = {'Description': description.format(point, albedo),
+                     'Point': point,
+                     'Albedo': albedo}
 
-        description = ("Accumulated solar irradiation for coordinate {} "
-                       "and albedo {}.")
-        attrs = {'Description': description.format(coordinate, albedo),
-                 'Coordinate': coordinate,
-                 'Albedo': albedo}
-        write_dataframe(df, dset_name, fid, compression, attrs=attrs)
+            transmittance = True if albedo == 't' else False
+            df = calculate_solar_radiation(flux_fnames[key], response_fname,
+                                           transmittance)
+            write_dataframe(df, dset_name, fid, compression, attrs=attrs)
 
     return
 
