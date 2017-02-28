fb94b5371eb254a43724cb2636f8c6e8f96e275b
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-02-28 15:25:09 +1100

    Update sys_variables.f90
    No longer builds as a private module.

--- gaip/sys_variables.f90
+++ gaip/sys_variables.f90
@@ -1,7 +1,6 @@
 module sys_variables
     implicit none
 
-    private
     public pi, d2r, r2d
     double precision, parameter :: pi = 4.0d0*atan(1.0d0)
     double precision, parameter :: d2r = pi/180.0d0
