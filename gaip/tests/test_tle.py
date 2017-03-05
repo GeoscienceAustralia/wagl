c32a1065e63cbb8002bab3ba74ec80d2d54c3094
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-03-06 09:08:37 +1100

    Updated TLE directory path for unittest.

--- gaip/tests/test_tle.py
+++ gaip/tests/test_tle.py
@@ -12,22 +12,22 @@ L7_DIR = os.path.join(DATA_DIR, 'L1T', 'LS7_90-84_2000-09-13', 'UTM',
 L8_DIR = os.path.join(DATA_DIR, 'L1T', 'LS8_90_84_2013-10-11', 'UTM',
                       'LS8_OLITIRS_OTH_P51_GALPGS01-002_090_084_20131011')
 
-TLE_DIR = '/g/data/v10/eoancillarydata/ephemeris_kill'
+TLE_DIR = '/g/data/v10/eoancillarydata/sensor-specific'
 
 class TLELoadingTest(unittest.TestCase):
 
     def test_load_tle_landsat5(self):
-        acq = gaip.acquisitions(L5_DIR)[0]
+        acq = gaip.acquisitions(L5_DIR).get_acquisitions(group='product')[0]
         data = gaip.load_tle(acq, TLE_DIR)
         self.assertIsInstance(data, ephem.EarthSatellite)
 
     def test_load_tle_landsat7(self):
-        acq = gaip.acquisitions(L7_DIR)[0]
+        acq = gaip.acquisitions(L7_DIR).get_acquisitions(group='product')[0]
         data = gaip.load_tle(acq, TLE_DIR)
         self.assertIsInstance(data, ephem.EarthSatellite)
 
     def test_load_tle_landsat8(self):
-        acq = gaip.acquisitions(L8_DIR)[0]
+        acq = gaip.acquisitions(L8_DIR).get_acquisitions(group='product')[0]
         data = gaip.load_tle(acq, TLE_DIR)
         self.assertIsInstance(data, ephem.EarthSatellite)
 
