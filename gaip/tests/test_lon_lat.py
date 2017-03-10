0ae350c7e2ffd07e6e8841da4a830769085a0cb6
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-03-11 00:12:12 +1100

    Import fix

--- gaip/tests/test_lon_lat.py
+++ gaip/tests/test_lon_lat.py
@@ -7,7 +7,7 @@ import numpy
 import numpy.testing as npt
 import osr
 
-from gaip.calculate_lat_lon_arrays import create_lon_lat_grids
+from gaip.calculate_lon_lat_arrays import create_lon_lat_grids
 from gaip import unittesting_tools as ut
 
 # WGS84
