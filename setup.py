64bbec06d0577c57ad9c36086055fa3f73538c5c
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-03-11 12:08:07 +1100

    Added commandline scripts to setup.py

--- setup.py
+++ setup.py
@@ -29,5 +29,12 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     url='https://github.com/GeoscienceAustralia/gaip',
     maintainer='Josh Sixsmith',
-    maintainer_email='joshua.sixsmith@ga.gov.au'
+    maintainer_email='joshua.sixsmith@ga.gov.au',
+    scripts=['scripts/test_calculate_angles.py',
+             'scripts/test_dsm.py',
+             'scripts/test_exiting_angles.py',
+             'scripts/test_incident_angles.py',
+             'scripts/test_relative_slope.py',
+             'scripts/test_shadow_masks.py',
+             'scripts/test_slope_aspect.py']
 )
