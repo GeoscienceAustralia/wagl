89b6f14d1685e08b0bed975d1d8461a2f808d7f4
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-03-13 07:24:44 +1100

    Removed old files.

--- workflow/tests/test_fc.sh
+++
@@ -1,3 +0,0 @@
-python ./fc.py FractionalCoverGenerator --local-scheduler \
---nbar-path /g/data1/rs0/scenes/ARG25_V0.0/2009-01/LS5_TM_NBAR_P54_GANBAR01-002_092_086_20090115 \
---output-base-dir /short/v10/smr547/luigi_fc_testing
