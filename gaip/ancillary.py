8fba31333987f31af212e15317a2d0a1f87b51c6
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-03-29 15:59:22 +1100

    Exclude atmospheric profile records that are less than the sruface layer.

--- gaip/ancillary.py
+++ gaip/ancillary.py
@@ -231,8 +231,12 @@ def collect_sbt_ancillary(acquisition, lonlats, dewpoint_path,
         df['Temperature'].iloc[0] = kelvin_2_celcius(t2m[0])
         df['Relative_Humidity'].iloc[0] = sfc_rh
 
+        # MODTRAN requires the height to be ascending
+        # remove any records that are less than the surface level
+        subset = df[df['GeoPotential_Height'] >= sfc_hgt[0]]
+
         dname = ppjoin(POINT_FMT.format(p=i), 'atmospheric-profile')
-        write_dataframe(df, dname, fid, compression, attrs=attrs)
+        write_dataframe(subset, dname, fid, compression, attrs=attrs)
 
     return fid
 
