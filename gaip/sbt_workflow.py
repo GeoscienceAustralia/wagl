5a414f100461f5548f3a94164f1ef1b6ff08709d
Ben Lewis <ben.lewis@ga.gov.au>
2017-04-03 15:40:57 +1000

    debugging
    plumbs SBT luigi task toward the components that fail

--- gaip/sbt_workflow.py
+++ gaip/sbt_workflow.py
@@ -21,7 +21,7 @@ from gaip.calculate_angles import _calculate_angles
 from gaip.calculate_incident_exiting_angles import _incident_angles
 from gaip.calculate_incident_exiting_angles import _exiting_angles
 from gaip.calculate_incident_exiting_angles import _relative_azimuth_slope
-from gaip.calculate_lon_lat_arrays import create_lon_lat_grids 
+from gaip.calculate_lon_lat_arrays import create_lon_lat_grids
 from gaip import constants
 from gaip.hdf5 import create_external_link
 from gaip.modtran import _format_tp5, _run_modtran, _calculate_solar_radiation
@@ -130,7 +130,7 @@ class RunAtmospherics(luigi.WrapperTask):
     #     inputs = self.input()
     #     with self.output().temporary_path() as out_fname:
     #         with h5py.File(out_fname, 'w') as fid:
-                
+
 
 class SBTAccumulateSolarIrradiance(AccumulateSolarIrradiance):
 
@@ -171,14 +171,19 @@ class SBTBilinearInterpolation(BilinearInterpolation):
                           'granule': self.granule, 'group': self.group,
                           'band_num': band, 'factor': factor}
                 tasks[key] = BilinearInterpolationBand(**kwargs)
+                break # TODO: remove (debugging)
+            break # TODO: remove (debugging)
         return tasks
 
-
+@inherits(CalculateLonLatGrids)
 class SurfaceTemperature(luigi.Task):
 
     """Calculates surface brightness temperature."""
 
-    # TODO: Re-write the class contents (output, run)
+    def run(self):
+        raise NotImplementedError
+    def output(self):
+        pass
 
     def requires(self):
 
@@ -209,7 +214,7 @@ class SBT(luigi.WrapperTask):
             for granule in container.granules:
                 for group in container.groups:
                     yield SurfaceTemperature(scene, work_root, granule, group)
-
+                    raise StopIteration # TODO: remove (debugging)
 
 if __name__ == '__main__':
     luigi.run()
