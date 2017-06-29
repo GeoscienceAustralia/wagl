ffb4031aa8a8f74ef91f534e715dfc72fefc1c36
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-06-29 12:11:04 +1000

    Test fixes, and added additional tests.

--- test/test_hdf5.py
+++ test/test_hdf5.py
@@ -83,7 +83,7 @@ class HDF5Test(unittest.TestCase):
                 'VERSION': '0.1'}
 
         # insert the attribute into the data dict
-        for k, v in data.items():
+        for k, v in attrs.items():
             data[k] = v
 
         fname = 'test_scalar_dataset.h5'
@@ -104,7 +104,7 @@ class HDF5Test(unittest.TestCase):
             hdf5.write_scalar(self.scalar_data, 'scalar', fid, attrs=attrs)
 
             data = hdf5.read_scalar(fid, 'scalar')
-            self.assertEqual(data['timestamp'], attrs['timestamp'].isoformat())
+            self.assertEqual(data['timestamp'], attrs['timestamp'])
 
     def test_attach_attributes(self):
         """
@@ -156,24 +156,40 @@ class HDF5Test(unittest.TestCase):
         """
         Test the image attributes of the write_h5_image function.
         """
-        minmax = numpy.array([self.image_data.min(), self.image_data.max()])
         attrs = {'CLASS': 'IMAGE',
                  'IMAGE_VERSION': '1.2',
-                 'DISPLAY_ORIGIN': 'UL',
-                 'IMAGE_MINMAXRANGE': minmax}
+                 'DISPLAY_ORIGIN': 'UL'}
 
         fname = 'test_write_h5_image_attributes.h5'
         with h5py.File(fname, **self.memory_kwargs) as fid:
             hdf5.write_h5_image(self.image_data, 'image', fid)
             test = {k: v for k, v in fid['image'].attrs.items()}
+
+            # assertDictEqual can't compare a numpy array, so test elsewhere
+            del test['IMAGE_MINMAXRANGE']
+
             self.assertDictEqual(test, attrs)
+
+    def test_write_h5_image_minmax(self):
+        """
+        Test the IMAGE_MINMAXRANGE attribute is correct.
+        """
+        minmax = numpy.array([self.image_data.min(), self.image_data.max()])
+
+        fname = 'test_write_h5_image_minmax.h5'
+        with h5py.File(fname, **self.memory_kwargs) as fid:
+            hdf5.write_h5_image(self.image_data, 'image', fid)
+
+            test = fid['image'].attrs['IMAGE_MINMAXRANGE']
+
+            self.assertTrue((minmax == test).all())
 
     def test_attach_table_attributes(self):
         """
         Test the attach_table_attributes function.
         """
         attrs = {'CLASS': 'TABLE',
-                 'IMAGE_VERSION': '0.2',
+                 'VERSION': '0.2',
                  'TITLE': 'Table',
                  'FIELD_0_NAME': 'float_data',
                  'FIELD_1_NAME': 'integer_data'}
