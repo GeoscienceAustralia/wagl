2efe847e226012cc6ae028c820720b1a60790f62
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-07-03 15:03:57 +1000

    Updates to the mtl data file locations.

--- tests/test_tle.py
+++ tests/test_tle.py
@@ -1,19 +1,18 @@
 from __future__ import absolute_import
+from os.path import join as pjoin, abspath, dirname
 import unittest
 import ephem
-import os
 from gaip.acquisition import acquisitions
 from gaip.tle import load_tle
 
-DATA_DIR = TLE_DIR = os.path.join(os.path.dirname(__file__), 'data')
-L5_MTL1 = os.path.join(DATA_DIR, 'L5090081_08120090407_MTL.txt')
-L5_MTL2 = os.path.join(DATA_DIR, 'LT05_L1TP_095066_20100601_20170222_01_T1_MTL.txt')
-L7_MTL1 = os.path.join(DATA_DIR, 'L71090081_08120090415_MTL.txt')
-L7_MTL2 = os.path.join(DATA_DIR, 'LE07_L1TP_112066_20020218_20170221_01_T1_MTL.txt')
-L8_MTL1 = os.path.join(DATA_DIR, 'LO80900842013284ASA00_MTL.txt')
-L8_MTL2 = os.path.join(DATA_DIR, 'LO80900842013284ASA00_MTL.txt')
+DATA_DIR = TLE_DIR = pjoin(dirname(abspath(__file__)), 'data')
+L5_MTL1 = pjoin(DATA_DIR, 'LANDSAT5', 'L5090081_08120090407_MTL.txt')
+L5_MTL2 = pjoin(DATA_DIR, 'LANDSAT5', 'LT05_L1TP_095066_20100601_20170222_01_T1_MTL.txt')
+L7_MTL1 = pjoin(DATA_DIR, 'LANDSAT7', 'L71090081_08120090415_MTL.txt')
+L7_MTL2 = pjoin(DATA_DIR, 'LANDSAT7', 'LE07_L1TP_112066_20020218_20170221_01_T1_MTL.txt')
+L8_MTL1 = pjoin(DATA_DIR, 'LANDSAT8', 'LO80900842013284ASA00_MTL.txt')
+L8_MTL2 = pjoin(DATA_DIR, 'LANDSAT8', 'LC80990842016277LGN00_MTL.txt')
 
-#TLE_DIR = '/g/data/v10/eoancillarydata/sensor-specific'
 
 class TLELoadingTest(unittest.TestCase):
 
