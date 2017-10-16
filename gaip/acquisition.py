59c70ec308fd7167ca89223a031a85e771efc655
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-10-16 15:34:15 +1100

    Changes to the band name and id for Sentinel-2a band 8A.

--- gaip/acquisition.py
+++ gaip/acquisition.py
@@ -712,7 +712,7 @@ def acquisitions_via_safe(pathname):
 
     # safe archive for S2a has two band name mappings, and we need to map ours
     band_map = {'0': '1', '1': '2', '2': '3', '3': '4', '4': '5', '5': '6',
-                '6': '7', '7': '8', '8': '8a', '9': '9', '10': '10',
+                '6': '7', '7': '8', '8': '8A', '9': '9', '10': '10',
                 '11': '11', '12': '12'}
 
     # conversion factors
