2341100195a9c523070da0e15f3c35eb4ff1ddfb
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-10-29 20:41:42 +1100

    Converted to uppercase for the parameters group.

--- gaip/satellite_solar_angles.py
+++ gaip/satellite_solar_angles.py
@@ -650,7 +650,7 @@ def _store_parameter_settings(fid, spheriod, orbital_elements,
     An internal function for storing the parameter settings for the
     calculate_angles workflow.
     """
-    group = fid.create_group('parameters')
+    group = fid.create_group('PARAMETERS')
     attach_attributes(group, params)
 
     # sheroid
