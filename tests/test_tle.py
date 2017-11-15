af9f4e574bfd6715992f2492c5d382bda08be01d
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-11-15 21:01:55 +1100

    Updated to use the reduced resolution scenes in the tests data directory.

--- tests/test_tle.py
+++ tests/test_tle.py
@@ -6,44 +6,25 @@ from gaip.acquisition import acquisitions
 from gaip.tle import load_tle
 
 DATA_DIR = TLE_DIR = pjoin(dirname(abspath(__file__)), 'data')
-L5_MTL1 = pjoin(DATA_DIR, 'LANDSAT5', 'L5090081_08120090407_MTL.txt')
-L5_MTL2 = pjoin(DATA_DIR, 'LANDSAT5', 'LT05_L1TP_095066_20100601_20170222_01_T1_MTL.txt')
-L7_MTL1 = pjoin(DATA_DIR, 'LANDSAT7', 'L71090081_08120090415_MTL.txt')
-L7_MTL2 = pjoin(DATA_DIR, 'LANDSAT7', 'LE07_L1TP_112066_20020218_20170221_01_T1_MTL.txt')
-L8_MTL1 = pjoin(DATA_DIR, 'LANDSAT8', 'LO80900842013284ASA00_MTL.txt')
-L8_MTL2 = pjoin(DATA_DIR, 'LANDSAT8', 'LC80990842016277LGN00_MTL.txt')
+LS5_SCENE1 = pjoin(DATA_DIR, 'LANDSAT5', 'LS5_TM_OTH_P51_GALPGS01-002_090_081_20090407')
+LS7_SCENE1 = pjoin(DATA_DIR, 'LANDSAT7', 'LS7_ETM_OTH_P51_GALPGS01-002_090_081_20090415')
+LS8_SCENE1 = pjoin(DATA_DIR, 'LANDSAT8', 'LS8_OLITIRS_OTH_P51_GALPGS01-032_090_084_20131011')
 
 
-@unittest.skip('Requires refactoring')
 class TLELoadingTest(unittest.TestCase):
 
     def test_load_tle_l5_mtl1(self):
-        acq = acquisitions(L5_MTL1).get_acquisitions()[0]
-        data = load_tle(acq, TLE_DIR)
-        self.assertIsInstance(data, ephem.EarthSatellite)
-
-    def test_load_tle_l5_mtl2(self):
-        acq = acquisitions(L5_MTL2).get_acquisitions()[0]
+        acq = acquisitions(LS5_SCENE1).get_acquisitions()[0]
         data = load_tle(acq, TLE_DIR)
         self.assertIsInstance(data, ephem.EarthSatellite)
 
     def test_load_tle_l7_mtl1(self):
-        acq = acquisitions(L7_MTL1).get_acquisitions()[0]
-        data = load_tle(acq, TLE_DIR)
-        self.assertIsInstance(data, ephem.EarthSatellite)
-
-    def test_load_tle_l7_mtl2(self):
-        acq = acquisitions(L7_MTL2).get_acquisitions()[0]
+        acq = acquisitions(LS7_SCENE1).get_acquisitions()[0]
         data = load_tle(acq, TLE_DIR)
         self.assertIsInstance(data, ephem.EarthSatellite)
 
     def test_load_tle_l8_mtl1(self):
-        acq = acquisitions(L8_MTL1).get_acquisitions()[0]
-        data = load_tle(acq, TLE_DIR)
-        self.assertIsInstance(data, ephem.EarthSatellite)
-
-    def test_load_tle_l8_mtl2(self):
-        acq = acquisitions(L8_MTL2).get_acquisitions()[0]
+        acq = acquisitions(LS8_SCENE1).get_acquisitions()[0]
         data = load_tle(acq, TLE_DIR)
         self.assertIsInstance(data, ephem.EarthSatellite)
 
