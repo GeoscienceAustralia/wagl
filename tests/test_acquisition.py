9d603cbfb49cff99d439ecadd80d60a6b8c3be15
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-11-15 12:11:39 +1100

    Added spectral_filter_file tests to ensure the correct filename matching.

--- tests/test_acquisition.py
+++ tests/test_acquisition.py
@@ -121,6 +121,14 @@ class Landsat5Scene1AcquisitionTest(unittest.TestCase):
     def test_read(self):
         self.assertEqual(self.acqs[0].data()[70, 30], 65)
 
+    def test_spectral_filter_file_vsir(self):
+        self.assertEqual(self.acqs[0].spectral_filter_file,
+                         'landsat5_vsir.flt')
+
+    def test_spectral_filter_file_thermal(self):
+        self.assertEqual(self.acqs[5].spectral_filter_file,
+                         'landsat5_thermal.flt')
+
 
 class Landsat7Mtl1AcquisitionTest(unittest.TestCase):
 
@@ -186,6 +194,14 @@ class Landsat7Mtl1AcquisitionTest(unittest.TestCase):
 
     def test_read(self):
         self.assertEqual(self.acqs[0].data()[70, 30], 61)
+
+    def test_spectral_filter_file_vsir(self):
+        self.assertEqual(self.acqs[0].spectral_filter_file,
+                         'landsat7_vsir.flt')
+
+    def test_spectral_filter_file_thermal(self):
+        self.assertEqual(self.acqs[5].spectral_filter_file,
+                         'landsat7_thermal.flt')
 
 
 class Landsat8Mtl1AcquisitionTest(unittest.TestCase):
@@ -254,6 +270,14 @@ class Landsat8Mtl1AcquisitionTest(unittest.TestCase):
 
     def test_read(self):
         self.assertEqual(self.acqs[0].data()[70, 30], 11003)
+
+    def test_spectral_filter_file_vsir(self):
+        self.assertEqual(self.acqs[0].spectral_filter_file,
+                         'landsat8_vsir.flt')
+
+    def test_spectral_filter_file_thermal(self):
+        self.assertEqual(self.acqs[1].spectral_filter_file,
+                         'landsat8_thermal.flt')
 
 
 if __name__ == '__main__':
