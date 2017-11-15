90f8a3cc492057d7344e01029c256aa2ba55337b
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-11-15 14:42:46 +1100

    Updated tp5 tests to utilise the unittest module.

--- tests/test_reformat_tp5.py
+++ tests/test_reformat_tp5.py
@@ -2,96 +2,88 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
+from os.path import join as pjoin
+import unittest
+import tempfile
 
-from gaip.modtran_profiles import (
-    MIDLAT_SUMMER_ALBEDO, TROPICAL_ALBEDO,
-    MIDLAT_SUMMER_TRANSMITTANCE, TROPICAL_TRANSMITTANCE
-)
-
-
-def main():
-    with open('data/test-midlat-summer.tp5', 'w') as src:
-        src.write(MIDLAT_SUMMER_ALBEDO.format(albedo=0.0,
-                                              water=1.07000122070313,
-                                              ozone=0.28499999642372,
-                                              filter_function='landsat7_vsir.flt',
-                                              visibility=-0.02264800000000,
-                                              elevation=0.70900000000000,
-                                              sat_height=705.0,
-                                              sat_view=171.000748,
-                                              doy=212,
-                                              lat=-29.33856209871443,
-                                              lon=209.88857485506449,
-                                              time=23.73920805027778,
-                                              sat_azimuth=279.408417))
-    
-    
-    # TODO: get a tropical dataset
-    with open('data/test-tropical.tp5', 'w') as src:
-        src.write(TROPICAL_ALBEDO.format(albedo=0.0,
-                                         water=1.07000122070313,
-                                         ozone=0.28499999642372,
-                                         filter_function='landsat7_vsir.flt',
-                                         visibility=-0.02264800000000,
-                                         elevation=0.70900000000000,
-                                         sat_height=705.0,
-                                         sat_view=171.000748,
-                                         doy=212,
-                                         lat=-29.33856209871443,
-                                         lon=209.88857485506449,
-                                         time=23.73920805027778,
-                                         sat_azimuth=279.408417))
+from gaip.modtran_profiles import MIDLAT_SUMMER_ALBEDO
+from gaip.modtran_profiles import MIDLAT_SUMMER_TRANSMITTANCE
 
 
-    with open('data/test-midlat-summer-trans.tp5', 'w') as src:
-        src.write(MIDLAT_SUMMER_TRANSMITTANCE.format(albedo=0.0,
-                                                     water=1.07000122070313,
-                                                     ozone=0.28499999642372,
-                                                     filter_function='landsat7_vsir.flt',
-                                                     visibility=-0.02264800000000,
-                                                     elevation=0.70900000000000,
-                                                     sat_height=705.0,
-                                                     sat_view=171.000748,
-                                                     doy=212,
-                                                     sat_view_offset=180.0 - 171.000748))
-    
-    
-    # TODO: get a tropical dataset
-    with open('data/test-tropical-trans.tp5', 'w') as src:
-        src.write(TROPICAL_TRANSMITTANCE.format(albedo=0.0,
-                                                water=1.07000122070313,
-                                                ozone=0.28499999642372,
-                                                filter_function='landsat7_vsir.flt',
-                                                visibility=-0.02264800000000,
-                                                elevation=0.70900000000000,
-                                                sat_height=705.0,
-                                                sat_view=171.000748,
-                                                doy=212,
-                                                sat_view_offset=180.0 - 171.000748))
+class Tp5ReformatTest(unittest.TestCase):
 
+    """
+    Test that the string formatting matches the old
+    output method.
+    """
 
-    with open('data/TL_alb_0.tp5', 'r') as src:
-        ref_albedo = src.readlines()
+    def test_midlat_summer_albdo0(self):
+        """
+        Test a mid latitude summer albedo 0 profile.
+        """
+        self.maxDiff = None
 
-    with open('data/test-midlat-summer.tp5', 'r') as src:
-        test_albedo = src.readlines()
+        with open('data/TL_alb_0.tp5', 'r') as src:
+            ref_albedo = ''.join(src.readlines())
 
-    print("Testing mid lat albedo")
-    for i in range(len(ref_albedo)):
-        if not ref_albedo[i] == test_albedo[i]:
-            print("Line {} not equivilent".format(i))
+        with tempfile.TemporaryDirectory() as tmpdir:
 
-    with open('data/TL_alb_t.tp5', 'r') as src:
-        ref_trans = src.readlines()
+            out_fname = pjoin(tmpdir, 'test-midlat-summer.tp5')
+            with open(out_fname, 'w') as src:
+                kwargs = {'albedo': 0.0,
+                          'water': 1.07000122070313,
+                          'ozone': 0.28499999642372,
+                          'filter_function': 'landsat7_vsir.flt',
+                          'visibility': -0.02264800000000,
+                          'elevation': 0.70900000000000,
+                          'sat_height': 705.0,
+                          'sat_view': 171.000748,
+                          'doy': 212,
+                          'lat': -29.33856209871443,
+                          'lon': 209.88857485506449,
+                          'time': 23.73920805027778,
+                          'sat_azimuth': 279.408417,
+                          'binary': ' '}
 
-    with open('data/test-midlat-summer-trans.tp5', 'r') as src:
-        test_trans = src.readlines()
+                src.write(MIDLAT_SUMMER_ALBEDO.format(**kwargs))
 
-    print("Testing mid lat transmittance")
-    for i in range(len(ref_trans)):
-        if not ref_trans[i] == test_trans[i]:
-            print("Line {} not equivilent".format(i))
+            with open(out_fname, 'r') as src:
+                test_albedo = ''.join(src.readlines())
+
+        self.assertEqual(test_albedo, ref_albedo)
+
+    def test_midlat_summer_transmittance(self):
+        """
+        Test a mid latitude summer transmittance profile.
+        """
+        self.maxDiff = None
+
+        with open('data/TL_alb_t.tp5', 'r') as src:
+            ref_trans = ''.join(src.readlines())
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+
+            out_fname = pjoin(tmpdir, 'test-midlat-summer-trans.tp5')
+            with open(out_fname, 'w') as src:
+                kwargs = {'albedo': 0.0,
+                          'water': 1.07000122070313,
+                          'ozone': 0.28499999642372,
+                          'filter_function': 'landsat7_vsir.flt',
+                          'visibility': -0.02264800000000,
+                          'elevation': 0.70900000000000,
+                          'sat_height': 705.0,
+                          'sat_view': 171.000748,
+                          'doy': 212,
+                          'sat_view_offset': 180.0 - 171.000748,
+                          'binary': ' '}
+
+                src.write(MIDLAT_SUMMER_TRANSMITTANCE.format(**kwargs))
+
+            with open(out_fname, 'r') as src:
+                test_trans = ''.join(src.readlines())
+
+        self.assertEqual(test_trans, ref_trans)
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
