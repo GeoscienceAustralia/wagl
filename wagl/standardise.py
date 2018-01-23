ce34fe416ef74885e691c5355363e98e2cc24cf2
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-01-23 13:59:56 +1100

    Redefined to use key names as opposed to an index which provides better clarity for on the item details.

--- wagl/standardise.py
+++ wagl/standardise.py
@@ -176,19 +176,21 @@ def card4l(level1, model, vertices, method, pixel_quality, landsea, tle_path,
 
         # radiative transfer for each point and albedo
         for key in tp5_data:
+            point, albedo = key
 
-            log.info('Radiative-Transfer', point=key[0], albedo=key[1].value)
+            log.info('Radiative-Transfer', point=point, albedo=albedo.value)
             with tempfile.TemporaryDirectory() as tmpdir:
 
-                prepare_modtran(acqs, key[0], [key[1]], tmpdir, modtran_exe)
+                prepare_modtran(acqs, point, [albedo], tmpdir, modtran_exe)
 
                 # tp5 data
-                fname = pjoin(tmpdir, tp5_fmt.format(p=key[0], a=key[1].value))
+                fname = pjoin(tmpdir,
+                              tp5_fmt.format(p=point, a=albedo.value))
                 with open(fname, 'w') as src:
                     src.writelines(tp5_data[key])
 
-                run_modtran(acqs, inputs_grp, model, nvertices, key[0],
-                            [key[1]], modtran_exe, tmpdir, fid, compression)
+                run_modtran(acqs, inputs_grp, model, nvertices, point,
+                            [albedo], modtran_exe, tmpdir, fid, compression)
 
         # atmospheric components
         log.info('Components')
