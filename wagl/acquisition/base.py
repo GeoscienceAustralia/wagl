adf007313569959f980a5a3c3b71309f5a4ef94a
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-02-28 16:32:25 +1100

    Added a get_mode_resolution that retrieves acquisitions from the most resolution group with the most number of acquisitions.

--- wagl/acquisition/base.py
+++ wagl/acquisition/base.py
@@ -5,6 +5,7 @@ from os.path import join as pjoin
 from functools import total_ordering
 from pkg_resources import resource_stream
 
+import numpy
 import rasterio
 
 from ..geobox import GriddedGeoBox
@@ -176,6 +177,31 @@ class AcquisitionsContainer(object):
             if acqs:
                 break
         return acqs, group
+
+    def get_mode_resolution(self, granule=None):
+        """
+        Retrieve the resolution group name that contains the mode
+        resolution. 
+
+        :return:
+            A `tuple` of (`list`, group_name) where the `list`
+            contains the acquisitions of supported bands from
+            the mode resolution group, and group_name is the name
+            of the group that the acquisitions came from.
+        """
+        dtype = numpy.dtype([('group', 'O'), ('frequency', 'int64')])
+        data = numpy.zeros(len(self.groups), dtype=dtype)
+
+        # insert data
+        data['group'] = self.groups
+        counts = [len(self.get_acquisitions(grp, None, False)) for grp in
+                  self.groups]
+        data['frequency'] = counts
+
+        mode_grp = data['group'][data['frequency'].argmax()]
+        acqs = self.get_acquisitions(mode_grp, granule)
+
+        return acqs, mode_grp
 
     @property
     def supported_groups(self):
