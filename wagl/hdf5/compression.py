fb50a31d4985cdf94579d2cb9bd111a5258ab9ba
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-04-17 11:47:34 +1000

    Additional keyword for attr.asdict in order to preserve the python type and not the JSON style.

--- wagl/hdf5/compression.py
+++ wagl/hdf5/compression.py
@@ -117,7 +117,8 @@ class H5CompressionConfig(object):
                    attr.fields(self.__class__).compression_opts,
                    attr.fields(self.__class__).chunks,
                    attr.fields(self.__class__).shuffle)
-        kwargs = attr.asdict(self, filter=attr.filters.include(*include))
+        kwargs = attr.asdict(self, filter=attr.filters.include(*include),
+                             retain_collection_types=True)
         return kwargs
 
 
