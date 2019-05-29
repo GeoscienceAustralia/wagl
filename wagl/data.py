8841cf76db5d89e32f28405f0bc046ec110a04af
Josh Sixsmith <josh.sixsmith@gmail.com>
2019-05-30 00:30:19 +1000

    Updated comments, added an additional keyword to enable reading of additional pixels on each each.

--- wagl/data.py
+++ wagl/data.py
@@ -290,14 +290,14 @@ def write_img(array, filename, driver='GTiff', geobox=None, nodata=None,
             subprocess.check_call(cmd, cwd=dirname(filename))
 
 
-def read_subset(fname, ul_xy, ur_xy, lr_xy, ll_xy, bands=1):
+def read_subset(fname, ul_xy, ur_xy, lr_xy, ll_xy, edge_buffer=0, bands=1):
     """
     Return a 2D or 3D NumPy array subsetted to the given bounding
     extents.
 
     :param fname:
         A string containing the full file pathname to an image on
-        disk.
+        disk. OR an HDF5 Dataset (h5py.Dataset).
 
     :param ul_xy:
         A tuple containing the Upper Left (x,y) co-ordinate pair
@@ -322,6 +322,12 @@ def read_subset(fname, ul_xy, ur_xy, lr_xy, ll_xy, bands=1):
         in real world (map) co-ordinates.  Co-ordinate pairs can be
         (longitude, latitude) or (eastings, northings), but they must
         be of the same reference as the image of interest.
+
+    :param edge_buffer:
+        An integer indicating the additional number of pixels to read
+        along each edge of the subset. Useful for when additional data
+        might be required, such as for reprojection.
+        Default is 0 pixels on each edge.
 
     :param bands:
         Can be an integer of list of integers representing the band(s)
@@ -362,12 +368,10 @@ def read_subset(fname, ul_xy, ur_xy, lr_xy, ll_xy, bands=1):
     img_ll_x, img_ll_y = [int(v) for v in inv * ll_xy]
 
     # Calculate the min and max array extents
-    # The ending array extents have +1 to account for Python's
-    # [inclusive, exclusive) index notation.
-    xstart = min(img_ul_x, img_ll_x)
-    ystart = min(img_ul_y, img_ur_y)
-    xend = max(img_ur_x, img_lr_x) + 1
-    yend = max(img_ll_y, img_lr_y) + 1
+    xstart = min(img_ul_x, img_ll_x) - edge_buffer
+    ystart = min(img_ul_y, img_ur_y) - edge_buffer
+    xend = max(img_ur_x, img_lr_x) + edge_buffer
+    yend = max(img_ll_y, img_lr_y) + edge_buffer
 
     # Check for out of bounds
     if (((xstart < 0) or (ystart < 0)) or
