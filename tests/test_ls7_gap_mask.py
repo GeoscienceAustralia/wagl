6048920fca218a8603406ba809d2337c3cffc4bb
Josh Sixsmith <josh.sixsmith@gmail.com>
2019-09-19 12:37:36 +1000

    Additional tests to check that we can handle LS7 acquisitions that don't have gap_masks, i.e. pre SLC-OFF and GA's LPGS which doesn't produce gap_masks for SLC-OFF affected data.

--- tests/test_ls7_gap_mask.py
+++ tests/test_ls7_gap_mask.py
@@ -5,7 +5,7 @@ import unittest
 from wagl.acquisition import acquisitions
 
 
-from .data import LS7_GAP_MASK
+from .data import LS7_GAP_MASK, LS7_NO_GAP_MASK
 
 
 class GapMaskRadianceTest(unittest.TestCase):
@@ -72,3 +72,66 @@ class GapMaskRadianceTest(unittest.TestCase):
         count = mask.sum()
         self.assertEqual(count, 64747)
 
+
+class NoGapMaskRadianceTest(unittest.TestCase):
+    """
+    Test that the abscence of a gap mask has no effect on loading
+    the data.
+    """
+
+    def setUp(self):
+        self.acqs = acquisitions(LS7_NO_GAP_MASK).get_all_acquisitions()
+
+    def test_band8(self):
+        acq = self.acqs[0]
+        _ = acq.radiance_data()
+        count = acq._gap_mask.sum()
+        self.assertEqual(count, 0)
+
+    def test_band1(self):
+        acq = self.acqs[1]
+        _ = acq.radiance_data()
+        count = acq._gap_mask.sum()
+        self.assertEqual(count, 0)
+
+    def test_band2(self):
+        acq = self.acqs[2]
+        _ = acq.radiance_data()
+        count = acq._gap_mask.sum()
+        self.assertEqual(count, 0)
+
+    def test_band3(self):
+        acq = self.acqs[3]
+        _ = acq.radiance_data()
+        count = acq._gap_mask.sum()
+        self.assertEqual(count, 0)
+
+    def test_band4(self):
+        acq = self.acqs[4]
+        _ = acq.radiance_data()
+        count = acq._gap_mask.sum()
+        self.assertEqual(count, 0)
+
+    def test_band5(self):
+        acq = self.acqs[5]
+        _ = acq.radiance_data()
+        count = acq._gap_mask.sum()
+        self.assertEqual(count, 0)
+
+    def test_band61(self):
+        acq = self.acqs[6]
+        _ = acq.radiance_data()
+        count = acq._gap_mask.sum()
+        self.assertEqual(count, 0)
+
+    def test_band62(self):
+        acq = self.acqs[7]
+        _ = acq.radiance_data()
+        count = acq._gap_mask.sum()
+        self.assertEqual(count, 0)
+
+    def test_band7(self):
+        acq = self.acqs[8]
+        _ = acq.radiance_data()
+        count = acq._gap_mask.sum()
+        self.assertEqual(count, 0)
