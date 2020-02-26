a2663d7ff69af0d816535d67b44f65b4a15f3441
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-02-26 17:18:36 +1100

    Updated logging configuration.

--- wagl/logs.py
+++ wagl/logs.py
@@ -1,13 +1,44 @@
+"""
+Logging configuration for wagl logs
+
+Defines structured logging for:
+    * Task message      -- qualname task
+    * Status messages   -- qualname status
+    * Luigi interface   -- qualname luigi-interface
+"""
+
 import logging
 
-from structlog import wrap_logger
+import structlog
 from structlog.processors import JSONRenderer
 
-ERROR_LOGGER = wrap_logger(logging.getLogger('errors'),
-                           processors=[JSONRenderer(indent=1, sort_keys=True)])
+
+COMMON_PROCESSORS = [
+    structlog.stdlib.add_log_level,
+    structlog.processors.TimeStamper(fmt="ISO"),
+    structlog.processors.StackInfoRenderer(),
+    structlog.processors.format_exc_info,
+    JSONRenderer(sort_keys=True)
+]
+
+
+def get_wrapped_logger(logger_name: str = 'root', **kwargs):
+    """ Returns a struct log equivalent for the named logger """
+    return structlog.wrap_logger(
+        logging.getLogger(logger_name),
+        COMMON_PROCESSORS,
+        **kwargs
+    )
+
+
+class FormatJSONL(logging.Formatter):
+    """ Prevents printing of the stack trace to enable JSON lines output """
+    def formatException(self, ei):
+        """ Disables printing separate stack traces """
+        return
+
+
+TASK_LOGGER = get_wrapped_logger('task', stack_info=True)
+STATUS_LOGGER = get_wrapped_logger('status')
 
 INTERFACE_LOGGER = logging.getLogger('luigi-interface')
-
-STATUS_LOGGER = wrap_logger(logging.getLogger('status'),
-                            processors=[JSONRenderer(indent=1, sort_keys=True)])
-
