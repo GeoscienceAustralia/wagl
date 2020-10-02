9b7ab37597c770a2b0a35217ee94b1d74228503d
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-10-02 11:39:32 +1000

    Updated based on suggestions by @jeremyh

--- wagl/__init__.py
+++ wagl/__init__.py
@@ -1,2 +1,13 @@
 from __future__ import print_function
 from __future__ import absolute_import
+
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata
+
+try:
+    __version__ = metadata.version(__name__)
+except metadata.PackageNotFoundError:
+    __version__ = "Not Installed"
