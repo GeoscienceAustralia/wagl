3fab43394eebc0b249a787e59e06e211367bf716
Duncan Gray <duncan.gray@ga.gov.au>
2021-03-11 16:58:40 +1100

    initial cut of ls7 rt c2 unit tests

--- tests/test_mtl.py
+++ tests/test_mtl.py
@@ -10,6 +10,7 @@ L5_MTL1 = pjoin(DATA_DIR, "LANDSAT5", "L5090081_08120090407_MTL.txt")
 L5_MTL2 = pjoin(DATA_DIR, "LANDSAT5", "LT05_L1TP_095066_20100601_20170222_01_T1_MTL.txt")
 L7_MTL1 = pjoin(DATA_DIR, "LANDSAT7", "L71090081_08120090415_MTL.txt")
 L7_MTL2 = pjoin(DATA_DIR, "LANDSAT7", "LE07_L1TP_112066_20020218_20170221_01_T1_MTL.txt")
+L7_MTLRTC2 = pjoin(DATA_DIR, "LANDSAT7", "LE71140812021051EDC00__C2_RT", "LE07_L1TP_114081_20210220_20210220_02_RT_MTL.txt")
 L8_MTL1 = pjoin(DATA_DIR, "LANDSAT8", "LO80900842013284ASA00_MTL.txt")
 L8_MTL2 = pjoin(DATA_DIR, "LANDSAT8", "LO80900842013284ASA00_MTL.txt")
 L8_MTL1C2 = pjoin(
@@ -175,6 +176,18 @@ class Landsat8MTRTC2ParserTest(unittest.TestCase):
         self.assertTrue("LEVEL1_THERMAL_CONSTANTS" in tree)
         self.assertTrue("LEVEL1_PROJECTION_PARAMETERS" in tree)
 
+class Landsat7MTRTC2ParserTest(unittest.TestCase):
+    def test_load(self):
+        tree = load_mtl(L7_MTLRTC2)
+        self.assertEqual(len(tree), 11)
+        self.assertTrue("PRODUCT_CONTENTS" in tree)
+        self.assertTrue("IMAGE_ATTRIBUTES" in tree)
+        self.assertTrue("LEVEL1_MIN_MAX_RADIANCE" in tree)
+        self.assertTrue("LEVEL1_MIN_MAX_REFLECTANCE" in tree)
+        self.assertTrue("LEVEL1_MIN_MAX_PIXEL_VALUE" in tree)
+        self.assertTrue("LEVEL1_RADIOMETRIC_RESCALING" in tree)
+        self.assertTrue("LEVEL1_THERMAL_CONSTANTS" in tree)
+        self.assertTrue("LEVEL1_PROJECTION_PARAMETERS" in tree)
 
 class Landsat8MTL2ParserTest(unittest.TestCase):
     def test_load(self):
